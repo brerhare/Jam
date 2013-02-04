d4aaec7698d1ca95d32bb82e6f08b636445035a3
kim <kim@spleen.(none)>
2013-02-04 16:23:02 +0000

    Implemented product category filter

--- www/yii/glitzaratti.com/protected/views/product/index.php
+++ www/yii/glitzaratti.com/protected/views/product/index.php
@@ -15,6 +15,25 @@ $this->menu=array(
 );
 ?>
 
+<script>
+function refreshDisplay()
+{
+	var inputs = document.getElementsByTagName("input");
+	var checkList = '';
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == "checkbox") {
+			if (inputs[i].checked) {
+				checkList += inputs[i].value.substr(4) + '|';
+			}
+		}
+	}
+	/* @@TODO: Remove this evil EVIL hack ... */
+	url = 'http://www.glitzaratti.com/index.php' + '/product/index?category_id=' + checkList;
+	//alert(url);
+	this.document.location.href = url;
+}
+</script>
+
 <style>
         /* Glitz specific */
     .Normal-P
@@ -48,28 +67,36 @@ $this->menu=array(
             <!-- Category checkboxes -->
 			<div class="Big-P">Filter your view</div>
 		    <?php foreach ($categories as $category): ?>
+				<?php $chk='';?>
 				<?php if (in_array($category->id, $catArray) || ($catArray[0] == 'all')):?>
-					<p class="Normal-P">
-                        <input type="checkbox" name="cat-<?php echo $category->id;?>" value="cat-<?php echo $category->id;?>">
-	                <?php echo $category->name;?>
-					</p>
-				<?php endif ?>
+					<?php $chk=' checked ';?>
+				<?php endif;?>
+				<p class="Normal-P">
+				<input type="checkbox" id="cat-<?php echo $category->id;?>" <?php echo $chk;?> value="cat-<?php echo $category->id;?>" onClick="refreshDisplay();">
+				<?php echo $category->name;?>
+				</p>
 		    <?php endforeach; ?>
 		</td>
 		<td style="vertical-align:top; width:400px">
 			<?php foreach ($categories as $category): ?>
-				<?php foreach ($category->products as $product): ?>
-					<?php foreach ($product->images as $image): ?>
-					<table><tr><td style="vertical-align:top; width:125px">
-						<a href="<?php echo $this->createUrl('product/view?id=' . $product->id);?>"><img src="/userdata/image/gall_<?php echo $image->filename?>" longdesc="<?php echo $this->createUrl('product/view?id=' . $product->id);?>" alt="<?php echo $product->name?>" width=120/></a><br/>
-					</td><td>
-						<div class=Medium-P><?php echo $product->name;?></div>
-						<p class=Normal-P><?php echo $product->description?></p>
-
-					</td></tr></table>
-						<?php break; /* Only show 1 image for each product */ ?>
+				<?php if (in_array($category->id, $catArray) || ($catArray[0] == 'all')):?>
+					<?php foreach ($category->products as $product): ?>
+						<?php foreach ($product->images as $image): ?>
+							<table>
+								<tr>
+									<td style="vertical-align:top; width:125px">
+										<a href="<?php echo $this->createUrl('product/view?id=' . $product->id);?>"><img src="/userdata/image/gall_<?php echo $image->filename?>" longdesc="<?php echo $this->createUrl('product/view?id=' . $product->id);?>" alt="<?php echo $product->name?>" width=120/></a><br/>
+									</td>
+									<td>
+										<div class=Medium-P><?php echo $product->name;?></div>
+										<p class=Normal-P><?php echo $product->description?></p>
+									</td>
+								</tr>
+							</table>
+							<?php break; /* Only show 1 image for each product */ ?>
+						<?php endforeach; ?>
 					<?php endforeach; ?>
-				<?php endforeach; ?>
+				<?php endif ?>
 			<?php endforeach; ?>
 		</td>
 	</tr>
