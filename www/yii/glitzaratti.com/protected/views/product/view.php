55ac68c1211cf5632fb35e6fdcc5536f25ebb0e7
kim <kim@spleen.(none)>
2013-02-06 16:23:11 +0000

    Tidied up html, no functional change

--- www/yii/glitzaratti.com/protected/views/product/view.php
+++ www/yii/glitzaratti.com/protected/views/product/view.php
@@ -83,87 +83,80 @@ $.fn.preload = function(){
 }
 </script>
 
-<!-- <h2 class="text-center">Product view</h2> -->
+<!-- Paypal button integration -->
+<div style="width:250px; display:block;">
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+		<input type="hidden" name="cmd" value="_xclick">
+		<input type="hidden" name="business" value="sales@glitzaratti.com">
+		<input type="hidden" name="lc" value="GB">
+		<!-- Default item_name and amount follow, but could be overridden by the dropdown list -->
+		<input type="hidden" name="item_name" value="<?php echo $model->name?>">
+		<input type="hidden" name="amount" value="<?php echo $model->price;?>">
+		<input type="hidden" name="item_number" value="<?php $model->id?>">
+		<input type="hidden" name="button_subtype" value="services">
+		<input type="hidden" name="no_note" value="0">
+		<input type="hidden" name="shipping" value="0.00">
+		<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+		<table >
+			<tr>
+				<td>
+<?php if (count($model->category->sizes)):?>
+					<p class="Buy-P">
+						<input type="hidden" name="on0" value="Size">Size
+					</p>
+				</td>
+				<td>
+					<select name="os0">
+	<?php foreach ($model->category->sizes as $size): ?>
+						<option value="<?php echo $size->text;?>"><?php echo $size->text;?></option>
+	<?php endforeach; ?>
+	<?php $listItemCount=0;?>
+	<?php foreach ($model->category->sizes as $size): ?>
+							<input type="hidden" name="option_select<?php echo $listItemCount;?>" value="<?php echo $size->text;?>">
+							<input type="hidden" name="option_amount<?php echo $listItemCount++;?>" value="<?php echo $model->price;?>">
+	<?php endforeach; ?>
+					</select>
+				</td>
+				<td>
+<?php endif;?>
+					<input type="image" src="http://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+				</td>
+			</tr>
+		</table>
+		<input type="hidden" name="currency_code" value="GBP">
+		<input type="hidden" name="option_index" value="0">
+	</form>
+</div>
 
 
-
-			<!-- Paypal button integration -->
-			<div style="width:250px; display:block;">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<input type="hidden" name="cmd" value="_xclick">
-				<input type="hidden" name="business" value="sales@glitzaratti.com">
-				<input type="hidden" name="lc" value="GB">
-				<!-- Default item_name and amount follow, but could be overridden by the dropdown list -->
-				<input type="hidden" name="item_name" value="<?php echo $model->name?>">
-				<input type="hidden" name="amount" value="<?php echo $model->price;?>">
-				<input type="hidden" name="item_number" value="<?php $model->id?>">
-				<input type="hidden" name="button_subtype" value="services">
-				<input type="hidden" name="no_note" value="0">
-				<input type="hidden" name="shipping" value="0.00">
-				<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-
-						<table >
-							<tr>
-								<td>
-
-				<?php if (count($model->category->sizes)):?>
-                                    <p class="Buy-P">
-                                        <input type="hidden" name="on0" value="Size">Size
-                                    </p>
-	                                    </td><td>
-									<select name="os0">
-										<?php foreach ($model->category->sizes as $size): ?>
-											<option value="<?php echo $size->text;?>"><?php echo $size->text;?></option>
-										<?php endforeach; ?>
-										<?php $listItemCount=0;?>
-										<?php foreach ($model->category->sizes as $size): ?>
-											<input type="hidden" name="option_select<?php echo $listItemCount;?>" value="<?php echo $size->text;?>">
-											<input type="hidden" name="option_amount<?php echo $listItemCount++;?>" value="<?php echo $model->price;?>">
-										<?php endforeach; ?>
-									</select>
-										</td><td>
-				<?php endif;?>
-                                    <input type="image" src="http://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-								</td>
-							</tr>
-						</table>
-				<input type="hidden" name="currency_code" value="GBP">
-				<input type="hidden" name="option_index" value="0">
-			</form>
-				</div>
-
-
-
-
-
-
-	<div id="gallery">
-        <center>
-        <p class="Normal-P">
-			<?php echo nl2br($model->name . ' - £' . $model->price .'<br>' . $model->description)?>
-        </p>
-			</center>
-		<p></p>
-		<table><tr><td style="vertical-align:top; width:150px">
-
-            <ul>
-				<?php foreach ($model->images as $image): ?>
-                <li><img src="/userdata/image/gall_<?php echo $image->filename;?>" alt="/userdata/image/<?php echo $image->filename;?>" style="padding:5px" width='150'/></li>
-				<?php endforeach; ?>
-            </ul>
-
-			</td><td style="vertical-align:top">
-
-		<span class='zoom' id='zoom-container'>
-			<p>Hover</p>
-				<?php foreach ($model->images as $image): ?>
+<div id="gallery">
+	<center>
+		<p class="Normal-P">
+<?php echo nl2br($model->name . ' - £' . $model->price .'<br>' . $model->description)?>
+		</p>
+	</center>
+	<p></p>
+	<table>
+		<tr>
+			<td style="vertical-align:top; width:150px">
+				<ul>
+<?php foreach ($model->images as $image): ?>
+					<li><img src="/userdata/image/gall_<?php echo $image->filename;?>" alt="/userdata/image/<?php echo $image->filename;?>" style="padding:5px" width='150'/></li>
+<?php endforeach; ?>
+				</ul>
+			</td>
+			<td style="vertical-align:top">
+				<span class='zoom' id='zoom-container'>
+					<p>Hover</p>
+<?php foreach ($model->images as $image): ?>
 					<img src="/userdata/image/<?php echo $image->filename;?>" height='500' id='gallery-image'/>
-					<?php break; ?>
-				<?php endforeach; ?>
-
-		</span>
-			</td></tr></table>
-	</div>
+<?php break; ?>
+<?php endforeach; ?>
+				</span>
+			</td>
+		</tr>
+	</table>
+</div>
 
 <!-- start of a thumbs-down-the-left container to replace the above
 <div class="grid">
