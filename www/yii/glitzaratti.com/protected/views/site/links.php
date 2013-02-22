be4ee3655f31cd014bafcd0b0b013b10725e4ffc
kim <kim@spleen.(none)>
2013-02-22 21:58:36 +0000

    Add text to links page

--- www/yii/glitzaratti.com/protected/views/site/links.php
+++ www/yii/glitzaratti.com/protected/views/site/links.php
@@ -31,6 +31,18 @@ ul {list-style:none;}
 
 
 <h4 class="Header-P"><<p class="Normal-P">About Us</p></h4>
+<p class=Buy-P style="font-size: 110%">
+Glitzaratti is an established clothing and design company based in Scotland. We specialise in shoe and clutch bag embellishment design.
+<br><br>
+All our products are hand made to order and therefore can be customised to suit our clients requirements from colours, shoe styles and those finishing touches like putting your initials or a special date to the shoe. Our aim is to produce you a unique piece of artwork footwear that says something about you.
+<br><br>
+we do commissions so if you have an idea in mind get in touch and we will make it happen.
+<br><br>
+We also run a
+    <a style="color:#ede587" href="<?php echo $this->createUrl('site/sendyourown');?>" ><span>'Supply Your Own'</span></a>
+service where you can supply your own shoes for perfect fit and comfort and let our design team transform them into a work of art that you will never want to take off.
+</p>
+<br>
 <p class='Normal-P'>
 Read about us
 <br>
