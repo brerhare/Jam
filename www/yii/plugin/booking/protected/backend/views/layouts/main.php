8ef5517c53e61a90230b74b3579b73a4c7f9734e
Kim <kim@wireflydesign.com>
2013-03-26 21:56:55 +0000

    Minor cosmetic changes to stdapp

--- www/yii/plugin/booking/protected/backend/views/layouts/main.php
+++ www/yii/plugin/booking/protected/backend/views/layouts/main.php
@@ -71,9 +71,8 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Wirefly Design.<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
