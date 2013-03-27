4e3414c80123ae48a6f174a396c830fafde26f5c
Kim <kim@wireflydesign.com>
2013-03-27 23:38:48 +0000

    insightklg header/footer done

--- www/yii/insightklg.co.uk/protected/views/layouts/main.php
+++ www/yii/insightklg.co.uk/protected/views/layouts/main.php
@@ -12,70 +12,61 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+<!--	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" /> -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+
+<style type="text/css">
+body{margin:0;padding:0;}
+
+.Body-P
+{
+    margin:0.0px 0.0px 12.0px 0.0px; text-align:center; font-weight:400;
+}
+.Body-C
+{
+    font-family:"Verdana", sans-serif; color:#ffffff; font-size:13.0px; line-height:1.23em;
+}
+</style>
+
+
+
+
 </head>
 
-<body>
+<body text="#000000" style="background:transparent url(<?php Yii::app()->request->baseUrl ?>/img/blackboard_bg.jpg) repeat scroll top center; height:340px;">
+<div style="background-color:transparent;margin-left:auto;margin-right:auto;position:relative;width:960px;height:340px;">
+<map id="map0" name="map0">
+    <area shape="rect" coords="46,120,186,180" href="index.php" alt="">
+    <area shape="poly" coords="69,308,73,307,79,301,84,295,94,279,97,270,102,262,109,246,114,237,121,230,127,227,135,224,141,220,150,217,159,213,165,207,174,202,185,194,188,191,194,183,207,162,209,152,209,145,208,139,205,132,202,126,194,113,194,109,196,101,197,92,195,86,192,81,188,73,191,66,195,58,191,49,192,39,190,29,184,23,177,21,169,23,166,25,159,28,151,33,143,39,135,44,129,53,122,57,115,60,106,65,100,72,96,75,90,83,83,85,71,91,62,95,54,100,37,121,32,129,28,138,22,156,19,171,19,181,21,200,23,206,24,214,27,221,32,229,34,236,39,245,40,254,39,271,40,283,42,293,46,301,53,307,60,310" href="index.php" alt="">
+</map>
+<img src="<?php Yii::app()->request->baseUrl ?> /img/shell.png" border="0" width="229" height="327" title="" alt="Insight&#10;" usemap="#map0" style="position:absolute;left:128px;top:14px;">
+<img src="<?php Yii::app()->request->baseUrl ?> /img/heading_title1.png" border="0" width="529" height="53" title="" alt=" Training, Coaching &amp; Media&#10;" style="position:absolute;left:342px;top:65px;">
+<map id="map1" name="map1">
+    <area shape="poly" coords="1,76,2,73,3,66,3,60,0,50,0,78" href="index.php" alt="">
+</map>
+<img src="<?php Yii::app()->request->baseUrl ?> /img/heading_title2.png" border="0" width="554" height="106" alt="" usemap="#map1" style="position:absolute;left:334px;top:99px;">
+</div>
 
-<div class="container" id="page">
+<div id="content">
+</div>
 
-
-<div style="margin-top:50px">
-
-	<?php $this->widget('bootstrap.widgets.TbNavbar', array(
-	'type' => 'null', // null or 'inverse'
-	'brand' => 'Home',
-	'brandUrl' => array('/site/index'),
-	'collapse' => true, // requires bootstrap-responsive.css
-	'items' => array(
-		array(
-			'class' => 'bootstrap.widgets.TbMenu',
-			'items' => array(
-				array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
-				array('label' => 'Contact', 'url' => array('/site/contact')),
-				array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
-				array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
-			),
-		),
-		array(
-			'class' => 'bootstrap.widgets.TbMenu',
-			'htmlOptions' => array('class' => 'pull-right'),
-			'items' => array(
-				array('label' => 'Link', 'url' => '#'),
-				'---',
-				array('label' => 'Dropdown', 'url' => '#', 'items' => array(
-					array('label' => 'Action', 'url' => '#'),
-					array('label' => 'Another action', 'url' => '#'),
-					array('label' => 'Something else here', 'url' => '#'),
-					'---',
-					array('label' => 'Separated link', 'url' => '#'),
-				)),
-			),
-		),
-	),
-)); ?>
-
-
-        </div><!-- navbar margin adjust -->
-
-	<?php if (isset($this->breadcrumbs)): ?>
-		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-		'links' => $this->breadcrumbs,
-	)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> Wirefly Design.<br/>
-		All Rights Reserved.<br/>
-	</div><!-- footer -->
-
-</div><!-- page -->
+<div id="footer" text="#000000" style="background:transparent url('wpimages/wp8f23f5b6_06.jpg') repeat scroll top center; height:133px;">
+    <div style="background-color:transparent;margin-left:auto;margin-right:auto;position:relative;width:960px;height:133px;">
+        <img src="<?php Yii::app()->request->baseUrl ?> /img/footer_shell.png" border="0" width="86" height="108" title="" alt="Insight&#10;" style="position:absolute;left:404px;top:0px;">
+        <div style="position:absolute;left:351px;top:42px;width:192px;height:31px;overflow:hidden;">
+            <p class="Wp-Body-P"><span class="Body-C">Copyright</span></p>
+        </div>
+        <div style="position:absolute;left:479px;top:43px;width:314px;height:31px;overflow:hidden;">
+            <p class="Wp-Body-P"><span class="Body-C">Training, Coaching &amp; Media 2013</span></p>
+        </div>
+        <div style="position:absolute;left:347px;top:75px;width:414px;height:51px;overflow:hidden;">
+            <p class="Body-P"><span class="Body-C">Contact Details info@insightKLG</span></p>
+            <p class="Body-P"><span class="Body-C">Hosted and Designed by <a href="http://www.wireflydesign.com" target="_blank" style="text-decoration:underline;">Wirefly Design</a></span></p>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
--- www/yii/plugin/booking/protected/backend/views/layouts/main.php
+++ www/yii/plugin/booking/protected/backend/views/layouts/main.php
@@ -38,6 +38,7 @@
 				array('label' => 'Extras', 'url' => array('/extra/admin'), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Facilities', 'url' => array('/facility/admin'), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Occupancy Types', 'url' => array('/occupancyType/admin'), 'visible' => !Yii::app()->user->isGuest),
+				array('label' => 'T&Cs', 'url' => array('/site/index'), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
 				array('label' => 'Logout', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Contact', 'url' => array('/site/contact'), 'visible' => Yii::app()->user->isGuest),
