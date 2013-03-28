ab32ed5bcf8a5d892e1e088d53fd4511d43d51e0
Kim <kim@wireflydesign.com>
2013-03-28 09:30:18 +0000

    Redactor size change

--- www/yii/insightklg.co.uk/protected/backend/views/contentBlock/update.php
+++ www/yii/insightklg.co.uk/protected/backend/views/contentBlock/update.php
@@ -5,6 +5,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Update Content Block <?php echo $model->id; ?></h1>
+<h1>Update Content Block <?php //echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
