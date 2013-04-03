e47fc95f39f3db751eebe8acb39ae1f08a7d27d7
Kim <kim@wireflydesign.com>
2013-04-04 00:04:57 +0100

    Changed layout of booking price tables

--- www/yii/plugin/booking/protected/backend/views/room/_form_options.php
+++ www/yii/plugin/booking/protected/backend/views/room/_form_options.php
@@ -13,37 +13,56 @@
 		// the table will be 0-padding to the box
 		'htmlOptions' => array('class'=>'bootstrap-widget-table')
 	));? >
+-->
 
-	 <?php echo $form->checkBoxListRow($model, 'checkboxes', array(
+<!-- -->
+	 <?php echo $form->checkBoxListRow($model, 'uid', array(
 	'Option one is this and that—be sure to include why it\'s great',
 	'Option two can also be checked and included in form results',
 	'Option three can—yes, you guessed it—also be checked and included in form results',
 ), array('hint'=>'<strong>Note:</strong> Labels surround all the options for much larger click areas.')); ?>
+<!-- -->
 
+
+<!--
+<div class="control-group ">
+  <label class="control-label" for="TestForm_checkboxes">Checkboxes</label>
+  <div class="controls">
+< ?php echo CHtml::label('Your Interests:', 'interests'); ? >
+< ?php echo CHtml::checkBoxList('interests', '',
+	array('PHP', 'MySQL', 'JavaScript', 'CSS', 'Yii Framework')
+); ? >
+  </div>
+</div>
 -->
-    <table class="table">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Single</th>
-            <th>Double</th>
-            <th>Any</th>
-            <th>Per Adult</th>
-            <th>Per Child</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr class="odd">
-            <td>Room Only</td><td></td><td></td><td></td><td></td><td></td>
-        </tr>
-        <tr class="even">
-            <td>Bed & Breakfast</td><td></td><td></td><td></td><td></td><td></td>
-        </tr>
-        <tr class="odd">
-            <td>Dinner, Bed & Breakfast</td><td></td><td></td><td></td><td></td><td></td>
-        </tr>
-        </tbody>
-    </table>
+
+
+
+<br/>
+
+<div class="control-group ">
+  <label class="control-label" for="TestForm_checkboxes">Checkboxes</label>
+  <div class="controls">
+    <input id="ytTestForm_checkboxes" type="hidden" value="" name="TestForm[checkboxes]" />
+    <label class="checkbox">
+      <input id="TestForm_checkboxes_0" value="0" type="checkbox" name="TestForm[checkboxes][]" />
+      <label for="TestForm_checkboxes_0">Option one</label>
+    </label>
+    <label class="checkbox">
+      <input id="TestForm_checkboxes_1" value="1" type="checkbox" name="TestForm[checkboxes][]" />
+      <label for="TestForm_checkboxes_1">Option two</label>
+    </label>
+    <label class="checkbox">
+      <input id="TestForm_checkboxes_2" value="2" type="checkbox" name="TestForm[checkboxes][]" />
+      <label for="TestForm_checkboxes_2">Option three</label>
+    </label>
+
+    <p class="help-block"><strong>Note:</strong> Labels surround all the options for much larger click areas.</p>
+
+  </div>
+</div>
+
+
 <!--
 	< ?php $this->endWidget();? >
 -->
