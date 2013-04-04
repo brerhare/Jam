f3a316e0fa635c60870e0afab1337d7d9eb26cef
Kim <kim@wireflydesign.com>
2013-04-04 23:55:06 +0100

    Insightklg layout sorted - twitter

--- www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
+++ www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
@@ -1,20 +1,4 @@
-<?php
-?>
 
-
-<div style="background-color:transparent;margin-left:auto;margin-right:auto;position:relative;width:660px;height:100%;">
-  <table>
-    <tr>
-      <td class="Body-C" style="width:100px;vertical-align:top">
-        <!-- Menu -->
-      </td>
-      <td>
-        <div id="content" class="Body-C" style="padding-left:20px;width:600px">
-          <?php echo $model->content; ?>
-        </div>
-      </td>
-    </tr>
-  </table>
+<div class="Body-C">
+<?php echo $model->content; ?>
 </div>
-
-
