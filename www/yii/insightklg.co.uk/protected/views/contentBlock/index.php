63765255f093c6f182fe257aa6dcfcb666c5873a
Kim <kim@wireflydesign.com>
2013-04-05 02:00:28 +0100

    parameterised dark/light background for redactor

--- www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
+++ www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
@@ -1,4 +1,5 @@
+<!-- To reverse the light/dark background flip BOTH the class and style below -->
 
-<div class="Body-C">
+<div class="Body-C" style="/*background-color: #ffffff;padding:5px 15px*/">
 <?php echo $model->content; ?>
 </div>
