ed10bdd89b2b85a359e3ca8675f0940bafb1465c
Kim <kim@wireflydesign.com>
2013-04-05 07:48:09 +0100

    insightklg rounded corners

--- www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
+++ www/yii/insightklg.co.uk/protected/views/contentBlock/index.php
@@ -1,5 +1,22 @@
 <!-- To reverse the light/dark background flip BOTH the class and style below -->
 
-<div class="Body-C" style="/*background-color: #ffffff;padding:5px 15px*/">
-<?php echo $model->content; ?>
+<style>
+divX {
+width:400px;
+height:25px;
+background-color:#FFEEDD;
+border: 1px solid #FF8855;
+padding: 5px;
+}
+#div1 {
+-moz-border-radius: 10px;
+-webkit-border-radius: 10px;
+border-radius: 10px;
+}
+</style>
+
+<div id="div1" class="xxxBody-C" style="width:650px; background-color: #ffffff;padding:5px 15px">
+
+  <?php echo $model->content; ?>
+
 </div>
