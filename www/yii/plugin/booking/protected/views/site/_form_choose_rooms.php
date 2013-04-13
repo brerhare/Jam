cfc9da9bf82b2aebe8d44be813bbae7faabf8627
Kim <kim@wireflydesign.com>
2013-04-13 23:55:18 +0100

    Booking - form

--- www/yii/plugin/booking/protected/views/site/_form_choose_rooms.php
+++ www/yii/plugin/booking/protected/views/site/_form_choose_rooms.php
@@ -1,20 +1,51 @@
+<?php // Control variables
+$maxRooms = 3; // How many rooms can be booked
+$showDays = 14; // How many days to show on calendar grid
+//-------------------
+echo "<script>var maxRooms=" . $maxRooms . ";</script>";
+?>
+	
 <style>
     table td, table th {
         padding: 0px;
     }
+	.cweekday {
+		text-align:center;
+		background-color: #3d5266;
+		color:#cce6ff;
+        border-left:1px solid white;
+        border-right:1px solid white;
+        font-family:Helvetica, Arial, sans-serif;
+        font-weight:100;
+	}
+	.cweekend {
+		background-color: #1F2933;
+	}
+
 </style>
 
 <script>
-function onChangeRoomCount(){
+function searchRooms(){
     alert('x');
 }
+// Show adult/child lines for however many room
+function showTopPickLines(){
+    var e = document.getElementById("numRooms");
+    var rooms = e.options[e.selectedIndex].value;
+    for (var i = 0; i < maxRooms; i++)
+    {
+    	if (i < rooms)
+    		$("#roomLine_"+(i+1)).show();
+    	else
+    		$("#roomLine_"+(i+1)).hide();
+   	}
+}
 </script>
-
 
 <div class="row">
 	<div class="span1"></div>
 	<div class='well span6'>
-        <table>
+        <table id="topPick">
         <tbody>
             <tr>
                 <th>Rooms</th>
@@ -23,42 +54,49 @@ function onChangeRoomCount(){
                 <th>Children</th>
 	            <th></th>
             </tr>
-            <tr>
+            <?php for ($i = 0; $i < $maxRooms; $i++): ?>
+            <tr id="roomLine_<?php echo ($i+1);?>" <?php if ($i>0) echo " style='display:none' ";?>   >
                 <td>
-                    <select name="numRooms" id="numRooms" style="width: 50px">
+                <?php if ($i == 0): ?>
+                    <select name="numRooms" id="numRooms" style="width: 50px" onchange="showTopPickLines()">
                         <option value="1" selected="selected">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                     </select>
+                <?php endif;?>
                 </td>
-                <td>Room 1</td>
+                <td>Room <?php echo ($i + 1); ?> </td>
                 <td>
-                    <select name="numRooms" id="numAdults1" style="width: 50px">
+                    <select name="numAdults_<?php echo ($i+1)?>" id="numAdults_<?php echo ($i+1)?>" style="width: 50px">
+                        <option value="1" selected="selected">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="numChildren_<?php echo ($i+1)?>" id="numChildren_<?php echo ($i+1)?>" style="width: 50px">
+                        <option value="0" selected="selected">0</option>
                         <option value="1">1</option>
-                        <option value="2" selected="selected">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-                </td>
-                <td>
-                    <select name="numRooms" id="numChildren1" style="width: 50px">
-                        <option value="0">0</option>
-                        <option value="1" selected="selected">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
                         <option value="5">5</option>
                     </select>
                 </td>
-                <td class="button">
-                    <div class="button_button"><input type="submit" name="ctl05$ctl00$checkAvailability" value="Check Availability" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl05$ctl00$checkAvailability&quot;, &quot;&quot;, true, &quot;availability&quot;, &quot;&quot;, false, false))" id="ctl05_ctl00_checkAvailability" class="input"></div>
+                <td>
+                <?php if ($i == 0): ?>
+                    <?php echo CHtml::button("Search rooms",array('onclick'=>'js:searchRooms();')); ?>
+                <?php endif; ?>
                 </td>
 	        </tr>
+	        <?php endfor; ?>
         </tbody>
 	    </table>
 	</div>
 </div>
+
 
 <div class="row">
 	<div class="span8">
@@ -77,12 +115,15 @@ function onChangeRoomCount(){
 				$str = '+1 week';
 				$str = '+1 week';
 				$str = 'today';
-				$showDays = 14;
 				$timestamp = strtotime($str);
 				for ($i = 0; $i < $showDays; $i++)
 				{
-					echo '<th style="width:5%">';
-					echo date('D', $timestamp + ($i * (60 * 60 * 24)));
+					$day = date('D', $timestamp + ($i * (60 * 60 * 24)));
+					$dayClass = ' cweekday ';
+					if (($day == 'Sat') || ($day == 'Sun'))
+						$dayClass .= ' cweekend ';
+					echo "<th class='" . $dayClass . "' style='width:5%'>";
+					echo $day;
 					echo '</th>';
 				}
 				?>
@@ -93,7 +134,7 @@ function onChangeRoomCount(){
 				<?php
 				for ($i = 0; $i < $showDays; $i++)
 				{
-					echo '<th style="width:5%">';
+					echo "<th class='" . $dayClass . "' style='width:5%'>";
 					echo date('j', $timestamp + ($i * (60 * 60 * 24)));
 					echo '</th>';
 				}
@@ -105,7 +146,7 @@ function onChangeRoomCount(){
 				<?php
 				for ($i = 0; $i < $showDays; $i++)
 				{
-					echo '<th style="width:5%">';
+					echo "<th class='" . $dayClass . "' style='width:5%'>";
 					echo date('M', $timestamp + ($i * (60 * 60 * 24)));
 					echo '</th>';
 				}
