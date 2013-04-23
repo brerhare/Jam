68e91a90dc69dfad6afbc06625458dfc6df16a2e
Kim <kim@wireflydesign.com>
2013-04-23 12:48:08 +0100

    Ticket logo in book tickets heading

--- www/yii/plugin/ticket/protected/views/event/_form.php
+++ www/yii/plugin/ticket/protected/views/event/_form.php
@@ -5,6 +5,24 @@
 		'type'=>'horizontal',
 	)); ?>
 
+	<div class="row">
+		<div class="span5 well">
+			<table>
+				<tr>
+					<td width="75%">
+						<b><?php echo $model->title; ?></b>
+						<br>
+						<i><?php echo $model->date; ?></i>
+					</td>
+					<td width="25%">
+<?php Yii::log("TICKET FORM : image " . Yii::app()->session['uid'], CLogger::LEVEL_WARNING, 'system.test.kim'); ?>
+						<?php echo CHtml::image(Yii::app()->baseUrl.'/userdata/' . Yii::app()->session['uid'] . '/' . $model->ticket_logo_path);?>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
 	<?php $this->widget('bootstrap.widgets.TbTabs',array(
 		'type'=>'tabs',
 		'tabs' => array(
