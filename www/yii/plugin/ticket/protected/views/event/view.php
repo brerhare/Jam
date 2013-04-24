ce2b15d4cc8baa8df83b1b02837c0730bcb63823
Kim <kim@wireflydesign.com>
2013-04-24 20:26:33 +0100

    Revert "Ticket - cleaned up files for ticker orderform"
    This reverts commit 02d6bc862040f8b28470e70bc91a6faa16f1a444.

---
+++ www/yii/plugin/ticket/protected/views/event/view.php
@@ -0,0 +1,37 @@
+<?php
+$this->breadcrumbs=array(
+	'Events'=>array('index'),
+	$model->title,
+);
+
+$this->menu=array(
+	array('label'=>'List Event','url'=>array('index')),
+	array('label'=>'Create Event','url'=>array('create')),
+	array('label'=>'Update Event','url'=>array('update','id'=>$model->id)),
+	array('label'=>'Delete Event','url'=>'#','linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Event','url'=>array('admin')),
+);
+?>
+
+<h1>View Event #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('bootstrap.widgets.TbDetailView',array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'uid',
+		'title',
+		'date',
+		'address',
+		'post_code',
+		'ticket_logo_path',
+		'ticket_text',
+		'ticket_terms',
+		'active',
+		'active_start_date',
+		'active_start_time',
+		'active_end_date',
+		'active_end_time',
+		'ticket_vendor_id',
+	),
+)); ?>
