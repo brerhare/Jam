928d7b8e8fac10c44689d4796c582b79930028d9
Kim <kim@wireflydesign.com>
2013-04-24 20:08:12 +0100

    Revert "Ticket - linked frontned site to backend"
    This reverts commit 45d573254f0328595282b160edcddfde9b69f2de.

---
+++ www/yii/plugin/ticket/protected/backend/views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>
+<?php echo "Session id: ".Yii::app()->session->sessionID;?>
+</p>
