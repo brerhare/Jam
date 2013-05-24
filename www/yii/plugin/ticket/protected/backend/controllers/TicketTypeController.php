998d3ec2dcdbbcd4381236220e3f796e3b80637e
Kim <kim@wireflydesign.com>
2013-05-24 11:57:26 +0100

    Ticket - delete auth

--- www/yii/plugin/ticket/protected/backend/controllers/TicketTypeController.php
+++ www/yii/plugin/ticket/protected/backend/controllers/TicketTypeController.php
@@ -31,7 +31,7 @@ class TicketTypeController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','session','admin'),
+				'actions'=>array('create','update','session','admin','delete'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
