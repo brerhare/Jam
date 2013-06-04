59ada4c787bfe84dffc8e9b4c29baac708746eab
Kim <kim@wireflydesign.com>
2013-06-04 09:49:52 +0100

    Ticket - print from thank you screen

--- www/yii/plugin/ticket/protected/controllers/TicketController.php
+++ www/yii/plugin/ticket/protected/controllers/TicketController.php
@@ -306,7 +306,7 @@ class TicketController extends Controller
         // using the default layout 'protected/views/layouts/main.php'
         $this->render('thankyou',array(
                         /*'model'=>$model,*/
-                        'myticket'=>Yii::app()->basePath . '/../tktp/' . $rnd . '.pdf',
+                        'myticket'=>Yii::app()->baseUrl . '/../tktp/' . $rnd . '.pdf',
                         'somedata'=>array(1,2,3),
         ));
 	}
--- www/yii/plugin/ticket/protected/views/ticket/_formthankyou.php
+++ www/yii/plugin/ticket/protected/views/ticket/_formthankyou.php
@@ -2,4 +2,4 @@
 Thank you for your order<br>
 Your tickets have been emailed to you. Should you not receive them in the next few minutes please check your spam folder
 </h5>
-<p>You may also print your tickets <a href="https://plugin.wirefydesign.com/tktp/<?php echo $myticket; ?>.pdf" target="_blank"; >here</a> (if it does not open please use the refresh button)
+<p>You may also print your tickets <a href="https://plugin.wirefydesign.com/tktp/<?php echo $myticket; ?>" target="_blank"; >here</a> (if it does not open please use the refresh button)
