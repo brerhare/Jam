8b855c20b16820ce4f05d5039adf9ecb2621bb9f
Kim <kim@wireflydesign.com>
2013-07-23 00:51:41 +0100

    Jelly - megamenu

--- www/yii/jelly/scripts/jelly/addon/menu/megamenu/megamenu.php
+++ www/yii/jelly/scripts/jelly/addon/menu/megamenu/megamenu.php
@@ -172,8 +172,8 @@ END_OF_API_HTML;
 	{
 		var rect = document.getElementById("megaanchor1").getBoundingClientRect();
 		//alert(rect.left);
-		val = rect.left -700;
-		//document.getElementById("megamenu1").setAttribute("style", "margin-left:" + val.toString() + "px");	
+		val = rect.left - 700;
+		document.getElementById("megamenu1").setAttribute("style", "margin-left:" + val.toString() + "px");	
 		///alert('ddd');
 	}
 END_OF_API_JS;
