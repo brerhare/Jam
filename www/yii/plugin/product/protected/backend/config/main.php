0d9b60630a569f7b4f4a9093dd3d4c7933cd8115
Kim <kim@wireflydesign.com>
2013-08-14 13:14:57 +0100

    Product - cleanup

--- www/yii/plugin/product/protected/backend/config/main.php
+++ www/yii/plugin/product/protected/backend/config/main.php
@@ -17,7 +17,7 @@ return array(
     'viewPath' => $backend.'/views',
     'runtimePath' => $backend.'/runtime',
 
-	'name'=>'Booking Backend',
+	'name'=>'Product Backend',
 
 	// preloading 'log' component
 	// preloading 'bootstrap' component
