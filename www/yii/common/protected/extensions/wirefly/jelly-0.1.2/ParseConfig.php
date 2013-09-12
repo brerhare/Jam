0cb26d8087ad1b90bcd91e420b2343b0e0816b75
Kim <kim@wireflydesign.com>
2013-09-12 08:46:43 +0100

    Jelly - create v0.1.2

---
+++ www/yii/common/protected/extensions/wirefly/jelly-0.1.2/ParseConfig.php
@@ -0,0 +1,268 @@
+<?php
+class ParseConfig
+{
+
+	//public $DEBUG = true;
+	public $DEBUG = false;
+
+	/**
+	 * Source lifted from http://php.net/manual/en/function.parse-ini-file.php
+	 */
+ 
+	private function parse_ini ( $filepath ) {
+	    $ini = $this->preprocess_file( $filepath );
+	    if ( count( $ini ) == 0 ) { return array(); }
+	    $sections = array();
+	    $values = array();
+	    $globals = array();
+		$sectionHasItems = false;		// Allows empty sections
+	    $i = 0;
+	    foreach( $ini as $line ){
+	        $line = trim( $line );
+	        // Comments
+	        if ( $line == '' || $line{0} == ';' || $line{0} == '#')
+	        	continue;
+	        // Sections
+	        if ( $line{0} == '[' ) { 
+	            $sections[] = substr( $line, 1, -1 );
+				if (($i != 0) && ($sectionHasItems==false)) $values[ $i - 1 ][] = '';       // Guarantee at least one item in each section
+	            $i++;
+				$sectionHasItems = false;
+	            continue;
+	        }
+	        // Key-value pair
+	        list( $key, $value ) = explode( '=', $line, 2 );
+	        $key = trim( $key );
+	        $value = trim( $value );
+	        if ( $i == 0 ) {
+	            // Array values
+	            if ( substr( $line, -1, 2 ) == '[]' ) {
+	                $globals[ $key ][] = $value;
+	            } else {
+	                $globals[ $key ] = $value;
+	            }
+	        } else {
+				$sectionHasItems = true;
+	            // Array values
+	            if ( substr( $line, -1, 2 ) == '[]' ) {
+	                $values[ $i - 1 ][ $key ][] = $value;
+	            } else {
+					if (!isset($values[ $i - 1 ][ $key ]))
+	                	$values[ $i - 1 ][ $key ] = $value;
+					else
+						$values[ $i - 1 ][ $key . '___' . 2 ] = $value;
+	            }
+	        }
+	    }
+	    for( $j=0; $j<$i; $j++ ) {
+	    	if ($values[$j])
+	        $result[ $sections[ $j ] ] = $values[ $j ];
+	    }
+	    return $result + $globals;
+	}
+
+	/**
+	 * Preprocess an ini file
+	 * 1) Expand includes
+	 * 2) ...
+	 */
+
+	private function preprocess_file($filepath)
+	{
+		// Read the ini into an array
+		$ini = file($filepath);
+		if (count($ini) == 0) { return array(); }
+
+		// Merge any includes into the array
+		$newIni = array();
+		foreach( $ini as $line )	
+		{
+			array_push($newIni, $line);
+			$line = trim( $line );
+	        // Comments
+	        if ( $line == '' || $line{0} == ';' || $line{0} == '#')
+	        	continue;
+	        // Includes
+	        $incl = explode('=', $line, 2);
+	        if (trim($incl[0]) == 'include')
+			{
+				array_pop($newIni);
+				$fileToInclude = dirname($filepath) . '/' . trim($incl[1]) . '.jel';
+				$ini2 = file($fileToInclude);
+				foreach( $ini2 as $line2 )	
+					array_push($newIni, $line2);
+			}
+		}
+		$ini = $newIni;
+
+		return $ini;
+	}
+
+    /**
+     * Loads in the ini file specified in filename, and returns the settings in
+     * it as an associative multi-dimensional array
+     *
+     * @param string $filename          The filename of the ini file being parsed
+     * @param boolean $process_sections By setting the process_sections parameter to TRUE,
+     *                                  you get a multidimensional array, with the section
+     *                                  names and settings included. The default for
+     *                                  process_sections is FALSE
+     * @param string $section_name      Specific section name to extract upon processing
+     * @return array
+     * @throws Exception
+     */
+
+public $multiInclude = array();
+
+    public /*static*/ function parse($filename, $process_sections = true, $section = null)
+    {
+
+		$ini = $this->parse_ini($filename);
+//$ini = parse_ini_file($filename, $process_sections = true, $section = null);
+
+        if ($ini === false)
+            throw new Exception('Unable to parse ini file.');
+
+		$this->logMsg("... Raw array after parsing ...\n\n");
+		if ($this->DEBUG) var_dump($ini);
+
+        if (!$process_sections && $section)
+		{
+            $values = $process_sections ? $ini[$section] : $ini;
+            $result = self::_processSection($values);
+        }
+		else
+		{
+            $result = array();
+            foreach ($ini as $section => $values)
+			{
+                if (!is_array($values))
+                    continue;
+                unset($ini[$section]);
+                $expand = explode(':', $section);
+                if (count($expand) == 2)
+				{
+                    $section = trim($expand[0]);
+                    $source = trim($expand[1]);
+
+					// Store any global includes for later merging
+					if ($section == '*')
+					{
+						$this->multiInclude[$section] = $source;
+						continue;
+					}
+
+                    if (!isset($result[$source]))
+                        throw new Exception("Unable to expand $section from $source");
+                    $sectionResult = self::_processSection($values);
+                    $result[$section] = self::_mergeRecursive($result[$source], $sectionResult);
+                }
+				else
+                    $result[$section] = self::_processSection($values, $result);
+            }
+            $result += $ini;
+        }
+
+		$this->logMsg("... Depandancy-merged array ...\n\n");
+		if ($this->DEBUG) var_dump($result);
+        return $result;
+    }
+
+    /**
+     * Process a single section with values.
+     * @param array $values
+     * @return array With the result.
+     */
+    private /*static*/ function _processSection($values, $topResult)
+    {
+        $result = array();
+        foreach ($values as $key => $value) {
+            $keys = explode('.', $key);
+            $result = self::_recurseValue($result, $keys, $value);
+        }
+
+		// Merge in any global includes
+		if (array_key_exists('*', $this->multiInclude))
+		    $result = self::_mergeRecursive($topResult[$this->multiInclude['*']], $result);  
+
+        return $result;
+    }
+
+    /**
+     * Create the values recursively.
+     * @param array $array
+     * @param array $keys
+     * @param mixed $value
+     * @return array The original array, with changes.
+     */
+    private /*static*/ function _recurseValue($array, $keys, $value)
+    {
+        $key = array_shift($keys);
+        if (count($keys) > 0) {
+            if (!isset($array[$key])) {
+                $array[$key] = array();
+            }
+            $array[$key] = self::_recurseValue($array[$key], $keys, $value);
+        } else {
+            $array = self::_mergeValue($array, $key, $value);
+        }
+        return $array;
+    }
+
+    /**
+     * Merge a value with the previous value.
+     * @param array $array
+     * @param string $key
+     * @param mixed $value
+     * @return array The original array, with changes.
+     */
+    private /*static*/ function _mergeValue($array, $key, $value)
+    {
+        if (!isset($array[$key])) {
+            $array[$key] = $value;
+        } else {
+            if (is_array($value)) {
+                $array[$key] += $value;
+            } else {
+                $array[$key][] = $value;
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * Recursively merge arrays, as the PHP function does not overwrite values.
+     * @param type $left
+     * @param type $right
+     */
+    private /*static*/ function _mergeRecursive($left, $right) {
+        // merge arrays if both variables are arrays
+        if (is_array($left) && is_array($right)) {
+            // loop through each right array's entry and merge it into $a
+            foreach ($right as $key => $value) {
+                if (isset($left[$key])) {
+                    $left[$key] = self::_mergeRecursive($left[$key], $value);
+                } else {
+                    $left[$key] = $value;
+                }
+            }
+        } else {
+            // one of values is not an array
+            $left = $right;
+        }
+        return $left;
+    }
+
+	private function logMsg($msg, $indentLevel=0)
+	{
+		if ($this->DEBUG)
+		{
+			$indent = "";
+			while ($indentLevel--)
+				$indent .= "&nbsp&nbsp&nbsp&nbsp";
+			echo  nl2br($indent . $msg);
+		}
+	}
+
+}
+?>
---
+++ www/yii/common/scripts/jelly-0.1.2/addon/tab/rain/rain.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ * API for JQuery Rain Tabs
+ *
+ * URL: http://www.jqueryrain.com/?hqttCwkg
+ *
+ * Notes
+ * -----
+ * None
+ *
+ */
+
+class rain
+{
+	//Defaults
+	private $defaultValue = "900px";
+
+	public $apiOption = array(
+		"width" => "900px",
+	);
+
+	/*
+	 * Set up any code pre-requisites (onload/document-ready reqs)
+	 * Apply options
+	 * Return an array containing [0]localContent [1]globalContent
+	 */
+	public function init($options, $jellyRootUrl)
+	{
+//		var_dump( $options );
+
+		// Generate the content into the html, replacing any <substituteN> tags
+		$content = "";
+		foreach ($options as $opt => $val)
+		{
+			switch ($opt)
+			{
+				case "source":
+					if ($val == "db")
+					{
+						// If db based content
+						// @@NB: OI! hardcoded to jacquies here
+						$content .= "<div id='rain-wrapper'>";
+						$content .= "<ul id='rain-tabs'>";
+						$cnt = 0;
+						$tabItems = TabBlock::model()->findAll(array('order'=>'sequence'));
+						foreach ($tabItems as $tabItem):
+							$content .= "<li><a href='#rain-tab" . ++$cnt . "'>" . $tabItem->title . "</a></li>";
+						endforeach;
+						$content .= "</ul>";
+						$cnt = 0;
+						foreach ($tabItems as $tabItem):
+							$content .= "<div class='rain-tabcontent-container' id='rain-tab" . ++$cnt . "'>";
+							$content .= $tabItem->content;
+							$content .= "</div>";
+						endforeach;
+						$content .= "</div>";
+					}
+					else if ($val == "glob")
+					{
+						// get pattern
+						$pattern = $options['pattern'];
+						foreach (glob(Yii::app()->basePath . "/../" . $pattern) as $filename)
+						{
+							$content .= "<li>";
+							$content .= "<img src='" . dirname($pattern) . "/". basename($filename) . "' style='float: none; margin: 0px;' alt=''>";
+							$content .= "</li>";
+						}
+					}
+					break;
+				case "width":
+					$tmp = str_replace("<substitute-width>", "width:" . $val . ";", $this->apiHtml);		// Optional field
+					$this->apiHtml = $tmp;
+					break;
+				case "height":
+					$tmp = str_replace("<substitute-height>", "height:" . $val . ";", $this->apiHtml);	// Optional field
+					$this->apiHtml = $tmp;
+					break;
+				case "animation":
+					$tmp = str_replace("<substitute-animation>", "'" . $val . "'", $this->apiJs);
+					$this->apiJs = $tmp;
+					break;
+				default:
+					// Not all array items are action items
+			}
+		}
+
+		// Apply all defaults that werent overridden
+		// HTML
+		if (strstr($this->apiHtml, "<substitute-something>"))
+		{
+			$tmp = str_replace("<substitute-something>", $this->defaultValue, $this->apiHtml);
+			$this->apiHtml = $tmp;
+		}
+		// JS
+
+		// Substitute paths for includes
+		$tmp = str_replace("<substitute-path>", $jellyRootUrl, $this->apiHtml);
+		$this->apiHtml = $tmp;
+
+		// Insert the data
+		$html = str_replace("<substitute-data>", $content, $this->apiHtml);
+		$js = $this->apiJs;
+
+		$retArr = array();
+		$retArr[0] = $html;
+		$retArr[1] = $js;
+		return $retArr;
+	}
+
+	// @@TODO: Thought: maybe all sites use a jelly db, and each has their own table prefix? Could avoid a lot of hassle
+
+	private $apiHtml = <<<END_OF_API_HTML
+
+	<div id="jelly-rain-container">
+		<!--JQuery Rain-->
+		<link rel="stylesheet" type="text/css" href="<substitute-path>/rain.css" />
+		<script type="text/javascript" src="<substitute-path>/rain.js"></script>
+		<substitute-data>
+	</div> <!-- jelly-rain-container -->
+
+END_OF_API_HTML;
+
+	private $apiJs = <<<END_OF_API_JS
+
+	jQuery(document).ready(function($){
+		$('#tabs li a:not(:first)').addClass('inactive');
+		$('.rain-tabcontent-container:not(:first)').hide();	
+	
+		$('#tabs li a').click(function(){		
+			var t = $(this).attr('href');
+			if($(this).hasClass('inactive')){ //added to not animate when active
+				$('#tabs li a').addClass('inactive');		
+				$(this).removeClass('inactive');
+				$('.rain-tabcontent-container').hide();
+				$(t).fadeIn('slow');	
+			}			
+			return false;
+		}) //end click
+/* Fire the 'Equal Heights Plugin' for all items of class 'rain-tabcontent-container' */
+$(".rain-tabcontent-container").equalHeights();
+
+});
+
+/**
+ * Equal Heights Plugin
+ * Equalize the heights of elements. Great for columns or any elements
+ * that need to be the same size (floats, etc).
+ * 
+ * Version 1.0
+ * Updated 12/10/2008
+ *
+ * Copyright (c) 2008 Rob Glazebrook (cssnewbie.com) 
+ *
+ * Usage: $(object).equalHeights([minHeight], [maxHeight]);
+ * 
+ * Example 1: $(".cols").equalHeights(); Sets all columns to the same height.
+ * Example 2: $(".cols").equalHeights(400); Sets all cols to at least 400px tall.
+ * Example 3: $(".cols").equalHeights(100,300); Cols are at least 100 but no more
+ * than 300 pixels tall. Elements with too much content will gain a scrollbar.
+ * 
+ */
+(function($) {
+	return;		// KIM! @@TODO!!!!!
+	$.fn.equalHeights = function(minHeight, maxHeight) {
+		tallest = (minHeight) ? minHeight : 0;
+		this.each(function() {
+			if($(this).height() > tallest) {
+				tallest = $(this).height();
+			}
+		});
+		if((maxHeight) && tallest > maxHeight) tallest = maxHeight;
+		return this.each(function() {
+			$(this).height(tallest).css("overflow","auto");
+		});
+	}
+})(jQuery);
+/* End Equal Heights plugin */
+
+END_OF_API_JS;
+
+}
+?>
