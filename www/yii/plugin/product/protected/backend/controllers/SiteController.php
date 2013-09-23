a7a02a8396a7ef2c1581f27bcfc21b1678b92d15
Kim <kim@wireflydesign.com>
2013-09-23 01:34:17 +0100

    Product - hardcode jdirect backdoor

--- www/yii/plugin/product/protected/backend/controllers/SiteController.php
+++ www/yii/plugin/product/protected/backend/controllers/SiteController.php
@@ -72,6 +72,19 @@ class SiteController extends Controller
 		$this->render('contact',array('model'=>$model));
 	}
 
+    /**
+     * Displays the DIRECT login page
+     */
+// @@TODO: HARDCODED FOR JACQUIES ---------- REMOVE
+    public function actionJDirect()
+    {
+        Yii::app()->session['uid'] = 19;
+        $identity = new UserIdentity('wendy@jacquiesbeauty.co.uk', 'guinot');
+        $duration = 3600*24*14; // 14 days
+        Yii::app()->user->login($identity, $duration);
+        $this->redirect(array('site/index'));
+    }
+
 	/**
 	 * Displays the login page
 	 */
