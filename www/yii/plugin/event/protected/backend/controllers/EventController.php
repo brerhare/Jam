6a9ea0f77383478adeb49d7fd45528f53b08fef8
Kim <kim@wireflydesign.com>
2013-10-10 10:29:51 +0100

    Event - add controller changes

--- www/yii/plugin/event/protected/backend/controllers/EventController.php
+++ www/yii/plugin/event/protected/backend/controllers/EventController.php
@@ -37,7 +37,7 @@ class EventController extends Controller
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete','import'),
+				'actions'=>array('admin','delete','import','admin'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
