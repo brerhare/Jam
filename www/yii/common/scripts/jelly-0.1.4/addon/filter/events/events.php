2faac3eeb66b0d7ade713d646aaa50eaa04477f1
Kim <kim@wireflydesign.com>
2013-10-19 14:10:01 +0100

    Addon - events - add price band

---
+++ www/yii/common/scripts/jelly-0.1.4/addon/filter/events/events.php
@@ -0,0 +1,170 @@
+<?php
+//error_reporting(E_ALL | E_STRICT);
+/**
+ * API for Event filter
+ *
+ * Notes
+ * -----
+ * This will use the width and height of your container
+ */
+
+class events
+{
+    //Defaults
+    private $defaultDepartment = "";
+    private $defaultWidth = "100%";
+
+// Not used ...
+    public $apiOption = array(
+    );
+
+    /*
+     * Set up any code pre-requisites (onload/document-ready reqs)
+     * Apply options
+     * Return an array containing [0]localContent [1]globalContent
+     */
+    public function init($options, $jellyRootUrl)
+    {
+//      var_dump( $options );
+
+        // Generate the content into the html, replacing any <substituteN> tags
+        $content = "";
+        foreach ($options as $opt => $val)
+        {
+            switch ($opt)
+            {
+                case "department":
+                    $department = $val;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Apply all defaults that werent overridden
+
+        // HTML
+
+        // JS
+        if (strstr($this->apiJs, "<substitute-department>"))
+        {
+            $tmp = str_replace("<substitute-department>", $this->defaultDepartment, $this->apiJs);
+            $this->apiJs = $tmp;
+        }
+
+        // Insert the data
+        $content = "<div style='color:#575757;'>";      // Your basic solemn grey font color
+        $uid = Yii::app()->session['uid'];
+/*
+        // Duration band (always shown if exists)
+        $durations = DurationBand::model()->findAll(array('order'=>'max', 'condition'=>'uid=' . $uid));
+        if ($durations)
+        {
+            $content .= "<br>";
+            $content .= "<div class='filter-header'>Duration<br>";
+            $content .= "<div class='filter-detail'>";
+            foreach ($durations as $duration):
+                $match = false;
+                $content .= "<label class='checkbox'> ";
+                $content .= "<input name='duration[]' "; 
+                if ($match) $content .= " checked='checked' ";
+                $content .= "type='checkbox' value='" . $duration->id . "'>" . $duration->max . " mins";
+                $content .= "</label><br>";
+            endforeach;
+            $content .= "</div>";
+            $content .= "</div>";
+        }
+*/
+        // Price band (always shown if exists)
+        $prices  = PriceBand::model()->findAll(array('order'=>'id'));
+        if ($prices)
+        {
+            $content .= "<br>";
+            $content .= "<div class='filter-header'>Price Band<br>";
+            $content .= "<div class='filter-detail'>";
+            foreach ($prices as $price):
+                $match = false;
+                $content .= "<label class='checkbox'> ";
+                $content .= "<input name='price[]' "; 
+                if ($match) $content .= " checked='checked' ";
+                $content .= "type='checkbox' value='" . $price->id . "'>" . $price->name;
+                $content .= "</label><br>";
+            endforeach;
+            $content .= "</div>";
+            $content .= "</div>";
+        }
+/*
+       // Departments with features 
+        $departments  = Department::model()->findAll(array('order'=>'name', 'condition'=>'uid=' . $uid));
+        if ($departments)
+        {
+            foreach ($departments as $department):
+                $content .= "<br>";
+                $content .= "<div id='h' class='filter-header'> <a href='#' >" . $department->name . "</a><br>";
+                $features  = Feature::model()->findAll(array('order'=>'name', 'condition'=>'product_department_id=' . $department->id));
+                $content .= "<div id='d' class='filter-detail'>";
+                foreach ($features as $feature):
+                    $match = false;
+                    $content .= "<label class='checkbox'> ";
+                    $content .= "<input name='feature[]' "; 
+                    if ($match) $content .= " checked='checked' ";
+                    $content .= "type='checkbox' value='" . $feature->id . "'>" . $feature->name;
+                    $content .= "</label><br>";
+                endforeach;
+                $content .= "</div>";
+                $content .= "</div>";
+            endforeach;
+            //$content .= "</div>";
+        }
+*/
+        $content .= "</div>";
+        $html = str_replace("<substitute-data>", $content, $this->apiHtml);
+        $this->apiHtml = $html;
+
+
+        // This is kind of a standard replace
+        $this->apiHtml = str_replace("<substitute-path>", $jellyRootUrl, $this->apiHtml);
+
+        $retArr = array();
+        $retArr[0] = $this->apiHtml;
+        $retArr[1] = $this->apiJs;
+        return $retArr;
+    }
+    // @@TODO: Thought: maybe all sites use a jelly db, and each has their own table prefix? Could avoid a lot of hassle
+
+    private $apiHtml = <<<END_OF_API_HTML
+
+        <div id="jelly-products-filter-container">
+            <!--Products Filter-->
+            <link rel="stylesheet" type="text/css" href="<substitute-path>/events.css" />
+            <substitute-data>
+        </div>
+
+END_OF_API_HTML;
+
+    private $apiJs = <<<END_OF_API_JS
+
+    var isDet = 0;
+
+    jQuery(document).ready(function($){
+    });
+
+
+    $('.filter-detail').click(function(){
+        isDet = 1;
+        $('.filter-detail', this).toggle(); // p00f
+    });
+
+    $('.filter-header').click(function(){
+        if (isDet == 1)
+        {
+            isDet = 0;
+            return;
+        }
+        $('.filter-detail', this).toggle(); // p00f
+    });
+
+END_OF_API_JS;
+
+}
+?>
