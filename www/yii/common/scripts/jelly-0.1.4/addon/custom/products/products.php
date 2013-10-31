9f9b710490eb6d8e349d2642976cb0664e4bdd63
Kim <kim@wireflydesign.com>
2013-10-31 19:19:44 +0000

    .

--- www/yii/common/scripts/jelly-0.1.4/addon/custom/products/products.php
+++ www/yii/common/scripts/jelly-0.1.4/addon/custom/products/products.php
@@ -149,7 +149,7 @@ END_OF_API_JS_product_page_options_dropdown;
 
 		// Pick up the Cart cookie
 		$cookieValue = (string)Yii::app()->request->cookies['wfcart'];
-//echo 'cookie=' . $cookieValue . '<br>';
+echo 'cookie=' . $cookieValue . '<br>';
 		$cartArr = explode('|', $cookieValue);
 		if (count($cartArr) == 0)
 		{
