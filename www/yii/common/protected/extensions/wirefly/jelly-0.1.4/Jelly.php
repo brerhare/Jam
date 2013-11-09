470e2e0b2b622cff5fcc7f532d9fc6514412a7fd
Kim <kim@wireflydesign.com>
2013-11-09 03:23:44 +0000

    Event - date filter working

--- www/yii/common/protected/extensions/wirefly/jelly-0.1.4/Jelly.php
+++ www/yii/common/protected/extensions/wirefly/jelly-0.1.4/Jelly.php
@@ -342,10 +342,11 @@ Yii::log("REPEATING EVAL = " . $query , CLogger::LEVEL_WARNING, 'system.test.kim
                         {
 
 
-/*******************
+/*******************/
 							// @@TODO WILD SEASONS hardcoding
 							if ((isset($_GET['grade'])) && ($_GET['grade'] != ''))
 							{
+								die('x');
     							$criteria = new CDbCriteria;
 								$criteria->addCondition("event_id = " . $r->id);
 								$Ws = Ws::model()->find($criteria);
@@ -358,7 +359,7 @@ Yii::log("REPEATING EVAL = " . $query , CLogger::LEVEL_WARNING, 'system.test.kim
 										continue;
 
 							}
-********************/
+/********************/
 							/* @@TODO THIS IS HALF DONE. CANT WORK LIKE GRADE ABOVE AS IS MANY-MANY
 							          SO ITS COMMENTED OUT FOR NOW
 							if ((isset($_GET['pb'])) && ($_GET['pb'] != ''))
