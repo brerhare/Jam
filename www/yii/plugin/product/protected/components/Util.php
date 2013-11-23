73d1d9482621496b6ef63234c6bf671bc7740edb
root on bo <kim@wireflydesign.com>
2013-11-23 07:43:57 +0000

    Product - add Util

---
+++ www/yii/plugin/product/protected/components/Util.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Util contains mostly static functions
+ */
+
+class Util
+{
+	public function encrypt($str)
+	{
+		$key = "password to (en/de)crypt";
+		return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $str, MCRYPT_MODE_CBC, md5(md5($key))));
+	}
+
+	public function decrypt($str)
+	{
+		$key = "password to (en/de)crypt";
+		return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($str), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+	}
+}
