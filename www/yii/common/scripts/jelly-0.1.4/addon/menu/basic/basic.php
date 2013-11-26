75e47b9da3589e183727dcb37ee8b0adc6713a97
Kim <kim@wireflydesign.com>
2013-11-26 20:06:25 +0000

    Addon - menu customisation

--- www/yii/common/scripts/jelly-0.1.4/addon/menu/basic/basic.php
+++ www/yii/common/scripts/jelly-0.1.4/addon/menu/basic/basic.php
@@ -36,6 +36,86 @@ class basic
 					$tmp = str_replace("<substitute-orientation>", $val, $this->apiHtml);
 					$this->apiHtml = $tmp;
 					break;
+
+				case "font-size":
+						$val = str_replace("px", "", $val);
+					$this->apiHtml = str_replace("<substitute-font-size>",
+						"nav ul li a {font-size: " . $val . "px;}",
+						$this->apiHtml);
+					break;
+
+				case "menu-color":
+					$vals = explode(" ", $val);
+					if (count($vals == 1)) array_push($vals, $vals[0]);
+					$this->apiHtml = str_replace("<substitute-menu-color>",
+						"nav ul {
+							background: " . $vals[0] . ";
+							background: linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%);  
+							background: -moz-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+							background: -webkit-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+						}",	
+						$this->apiHtml);
+					break;
+				case "submenu-color":
+					$vals = explode(" ", $val);
+					if (count($vals == 1)) array_push($vals, $vals[0]);
+					$this->apiHtml = str_replace("<substitute-submenu-color>",
+						"nav ul ul {
+							background: " . $vals[0] . ";
+							background: linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%);  
+							background: -moz-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+							background: -webkit-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+						}",	
+						$this->apiHtml);
+					break;
+
+				case "item-color":
+					$vals = explode(" ", $val);
+					if (count($vals == 1)) array_push($vals, $vals[0]);
+					$this->apiHtml = str_replace("<substitute-item-color>",
+						"nav ul li:hover {
+							background: " . $vals[0] . ";
+							background: linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%);  
+							background: -moz-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+							background: -webkit-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+						}",	
+						$this->apiHtml);
+					break;
+				case "subitem-color":
+					$vals = explode(" ", $val);
+					if (count($vals == 1)) array_push($vals, $vals[0]);
+					$this->apiHtml = str_replace("<substitute-subitem-color>",
+						"nav ul ul li a:hover {
+							background: " . $vals[0] . ";
+							background: linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%);  
+							background: -moz-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+							background: -webkit-linear-gradient(top, " . $vals[0] . " 0%, " . $vals[1] . " 100%); 
+						}",	
+						$this->apiHtml);
+					break;
+
+				case "subitem-separator-color":
+					$this->apiHtml = str_replace("<substitute-subitem-separator-color>",
+						"nav ul ul li {border-top: 1px solid " . $val . ";}",
+						$this->apiHtml);
+					break;
+
+
+//				case "text-color":
+//					break;
+
+
+				case "item-text-color":
+					$this->apiHtml = str_replace("<substitute-item-text-color>",
+						"nav ul li:hover a {color: " . $val . ";}",
+						$this->apiHtml);
+					break;
+				case "subitem-text-color":
+					$this->apiHtml = str_replace("<substitute-subitem-text-color>",
+						"nav ul ul li a:hover {color: " . $val . ";}",
+						$this->apiHtml);
+					break;
+
 				default:
 					// Not all array items are action items
 			}
@@ -45,10 +125,15 @@ class basic
 		// HTML
 //@@TODO: vertical isnt handled yet, only the default horiz
 		if (strstr($this->apiHtml, "<substitute-orientation>"))
-		{
-			$tmp = str_replace("<substitute-orientation>", $this->defaultOrientation, $this->apiHtml);
-			$this->apiHtml = $tmp;
-		}
+			$this->apiHtml = str_replace("<substitute-orientation>", $this->defaultOrientation, $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-font-size>", "", $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-menu-color>", "", $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-submenu-color>", "", $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-item-color>", "", $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-item-text-color>", "", $this->apiHtml);		
+		$this->apiHtml = str_replace("<substitute-subitem-color>", "", $this->apiHtml);
+		$this->apiHtml = str_replace("<substitute-subitem-text-color>", "", $this->apiHtml);		
+		$this->apiHtml = str_replace("<substitute-subitem-separator-color>", "", $this->apiHtml);
 
 		// JS
 
@@ -99,6 +184,21 @@ class basic
 	<div id="jelly-basicmenu-container">
 		<!--Basic Menu includes -->
 		<link rel="stylesheet" type="text/css" href="<substitute-path>/basic.css" />
+
+<style>
+<substitute-font-size>
+<substitute-menu-color>
+<substitute-submenu-color>
+<substitute-item-color>
+<substitute-item-text-color>
+<substitute-subitem-color>
+<substitute-subitem-text-color>
+<substitute-subitem-separator-color>
+</style>
+
+
+
+
 
 		<!--Basic Menu HTML-->
 		<div id="basicmenu">
