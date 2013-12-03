8425808f63b1d1d4cd410137161c276b1f0a5c55
root on bo <kim@wireflydesign.com>
2013-12-03 13:43:34 +0000

    DFD - softlinks

--- www/yii/dumfriesfurniture.com/protected/backend/views/tabBlock/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->menu=array(
-	array('label'=>'Manage Tab Content','url'=>array('admin')),
-);
-?>
-
-<h1>Update Tab Content <?php echo $model->id; ?></h1>
-
-<?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
