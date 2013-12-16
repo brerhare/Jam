0aeab5e74e86004549dd0c5b1f2e77d8cdfce33b
Kim <kim@wireflydesign.com>
2013-12-16 21:12:57 +0000

    Jelly - added gallery model to common, jelly_skeleton and test

--- www/yii/common/protected/data/jelly.sql
+++ www/yii/common/protected/data/jelly.sql
@@ -117,6 +117,44 @@ CREATE  TABLE IF NOT EXISTS `jelly_download` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `jelly_gallery`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `jelly_gallery` ;
+
+CREATE  TABLE IF NOT EXISTS `jelly_gallery` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `sequence` INT NULL ,
+  `title` VARCHAR(255) NOT NULL ,
+  `image` VARCHAR(255) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `sequence` (`sequence` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `jelly_gallery_image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `jelly_gallery_image` ;
+
+CREATE  TABLE IF NOT EXISTS `jelly_gallery_image` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `sequence` INT NULL ,
+  `text` TEXT NULL ,
+  `image` VARCHAR(255) NOT NULL ,
+  `url` VARCHAR(255) NULL ,
+  `jelly_gallery_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `sequence` (`sequence` ASC) ,
+  INDEX `fk_jelly_gallery_image_jelly_gallery` (`jelly_gallery_id` ASC) ,
+  CONSTRAINT `fk_jelly_gallery_image_jelly_gallery`
+    FOREIGN KEY (`jelly_gallery_id` )
+    REFERENCES `jelly_gallery` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
