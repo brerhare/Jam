126d375fa6f8cbdc316ec50d55cee121d11fba77
Kim <kim@wireflydesign.com>
2014-07-01 09:51:03 +0100

    Demo - add demo.

--- www/yii/demo.wireflydesign.com/1/protected/backend/views/site/pages/about.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name . ' - About';
-?>
-<h1>About</h1>
-
-<p>Administration of Jacquies Beauty page content</p>
--- www/yii/demo.wireflydesign.com/1/protected/data/dbcreate.sh
+++
@@ -1,5 +0,0 @@
-echo "create database demo1_wireflydesign_com" | mysql -u root -p"Wole9anic-"
-
-echo "grant all privileges on demo1_wireflydesign_com.* to 'wireflydesign.co' identified by 'wireflydesign.com,'" | mysql -u root -p"Wole9anic-"
-
-
