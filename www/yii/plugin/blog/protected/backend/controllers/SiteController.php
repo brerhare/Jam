3e5919e83f4df8ad905d1962ff1ed38df09ddb93
root on bo <kim@wireflydesign.com>
2014-07-26 18:07:07 +0100

    Blog - add byycewalkervending

--- www/yii/plugin/blog/protected/backend/controllers/SiteController.php
+++ www/yii/plugin/blog/protected/backend/controllers/SiteController.php
@@ -79,6 +79,20 @@ class SiteController extends Controller
     /**
      * Displays the DIRECT login page
      */
+// @@TODO: HARDCODED FOR BRYCEWALKERVENDING ---------- REMOVE
+    public function actionBryceWalkerVendingDirect()
+    {
+        Yii::app()->session['uid'] = 70;
+        $identity = new UserIdentity('bryce@brycewalkervending.com', 'bubbles!');
+        $identity->authenticate();
+        $duration = 3600*24*14; // 14 days
+        Yii::app()->user->login($identity, $duration);
+        $this->redirect(array('site/index'));
+    }
+
+    /**
+     * Displays the DIRECT login page
+     */
 // @@TODO: HARDCODED FOR DEMO ---------- REMOVE
     public function actionDemoDirect()
     {
