340f2ffbd8bd379332612b14ab28abeddd7c34c5
root on bo <kim@wireflydesign.com>
2014-08-30 16:55:12 +0100

    Ticket - active=0 means no more ticket sales, yeah?

--- www/yii/plugin/ticket/protected/views/ticket/book.php
+++ www/yii/plugin/ticket/protected/views/ticket/book.php
@@ -1,1 +1,6 @@
-<?php echo $this->renderPartial('_form', array('model'=>$model, 'somedata'=>$somedata)); ?>
+<?php
+	if ($model->active)
+		echo $this->renderPartial('_form', array('model'=>$model, 'somedata'=>$somedata));
+	else
+		echo "<h6 style='color:#ff0000'><center>Sorry... ticket sales for this event are inactive. This usually means it has sold out or started :-(</center></h6>";
+?>
