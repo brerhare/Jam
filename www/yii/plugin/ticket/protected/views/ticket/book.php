c18b80733808b2bed659d959e9086adb8718dfdf
root on bo <kim@wireflydesign.com>
2014-08-31 15:41:09 +0100

    Ticket - sold out message changed

--- www/yii/plugin/ticket/protected/views/ticket/book.php
+++ www/yii/plugin/ticket/protected/views/ticket/book.php
@@ -2,5 +2,5 @@
 	if ($model->active)
 		echo $this->renderPartial('_form', array('model'=>$model, 'somedata'=>$somedata));
 	else
-		echo "<h6 style='color:#ff0000'><center>Sorry... ticket sales for this event are inactive. This usually means it has sold out or started :-(</center></h6>";
+		echo "<h6 style='color:#ff0000'><center>Sorry... ticket sales for this event are inactive. This usually means it has sold out or started</center></h6>";
 ?>
