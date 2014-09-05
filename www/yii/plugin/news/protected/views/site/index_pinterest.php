d692c79a817795224f5decc604dbfc5e24746c34
root on bo <kim@wireflydesign.com>
2014-09-05 12:55:45 +0100

    Plugin - news - make sidebar common to traditional and pinterest as an include

--- www/yii/plugin/news/protected/views/site/index_pinterest.php
+++ www/yii/plugin/news/protected/views/site/index_pinterest.php
@@ -57,76 +57,7 @@ opacity:0.85;
 <div style="padding-top:10px; width:100%" ng-app>
 
 <span style="vertical-align:top; padding:0px 10px 10px; margin:0px 10px; display:inline-block; width:20%; ">
-
-<?php
-    // Show the 3 most recent articles
-    echo "<div style='font-size:12px'>";
-    echo "<span style='padding:5px; background-color:#d3d3d3'>Recent&nbsp&nbsp</span><br/><br/>";
-    $criteria = new CDbCriteria;
-    $criteria->addCondition("uid=" . Yii::app()->session['uid']);
-    $criteria->order = "date DESC";
-    $articles = Article::model()->findAll($criteria);
-    $cnt = 0;
-    if ($articles)
-    {
-        foreach ($articles as $article)
-        {
-			echo "<a style='text-decoration:none;color:black' target='_top' href='http:/test.wireflydesign.com/?layout=index&page=news-traditional&cat=0&art=" . $article->id . "'>";
-            //echo "<a style='text-decoration:none;color:black' href='https://plugin.wireflydesign.com/news/index.php/site/play/?cat=0&art=" . $article->id . "'>";
-            echo $article->title . "<br/>";
-            echo "</a>";
-            if ($cnt++ > 3)
-                break;
-        }
-    }
-    echo "</div>";
-    echo "<br/>";
-?>
-
-<?php
-	// Show the category list
-	$criteria = new CDbCriteria;
-	$criteria->addCondition("uid=" . Yii::app()->session['uid']);
-	$criteria->order = "name ASC";
-	$categories = Category::model()->findAll($criteria);
-	if ($categories)
-	{
-		echo "<hr>";
-		foreach ($categories as $category)
-		{
-			if ($category->id == $showCat)
-				continue;
-			echo "<a style='color:black; text-decoration:none' href='https://plugin.wireflydesign.com/news/index.php/site/play/?cat=" . $category->id . "&art='>" . $category->name . "</a><br>";
-			echo "<hr>";
-		}
-		if (($showCat != "0") || ((isset($_GET['art'])) && ($_GET['art'] != '')))
-		{
-			echo "<a style='color:black; text-decoration:none' href='https://plugin.wireflydesign.com/news/index.php/site/play/?cat=0&art='>" . 'All' . "</a><br>";
-			echo "<hr>";
-		}
-	}
-	// Show the signup form (@@EG calling an addon directly, not via the jelly)
-	require(Yii::app()->basePath . "/../scripts/jelly/addon/mailer/signup/signup.php");
-	$addon = new signup;
-	$optArr = array();
-	$optArr['buttoncolor'] = 'white';
-	$optArr['buttontextcolor'] = '#a70055';
-	$optArr['buttontext'] = 'Sign up';
-	$optArr['inputspacing'] = '5px';
-	$optArr['successtextcolor'] = 'white';
-	$optArr['failuretextcolor'] = 'red';
-	$ret = $addon->init($optArr, '/news/scripts/jelly/addon/mailer/signup');
-	echo "<br/>";
-	echo "<div style='font-size:13px; padding:1px; background-color:lightgrey'>";
-	echo "Keep me informed<br/>";
-	echo $ret[0];
-	echo "</div>";
-	echo "<script>" . $ret[1] . "</script>";
-//echo"<script>SID='" . $_GET['sid'] . "';</script>";
-
-
-
-?>
+<?php require('_sidebar.php'); ?>
 </span>
 
 <span class="mainitem" style="display:inline-block; width:70%">
