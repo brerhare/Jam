568736d2e5b5bfeafb023dd5778aae0599b01ef2
root on bo <kim@wireflydesign.com>
2014-10-08 15:21:11 +0100

    Blog -> News

--- www/yii/test.wireflydesign.com/protected/backend/views/layouts/main.php
+++ www/yii/test.wireflydesign.com/protected/backend/views/layouts/main.php
@@ -40,7 +40,7 @@
                 array('label' => 'Tabs', 'url' => array('/tabBlock/admin'), 'visible' => !Yii::app()->user->isGuest),                      
                 array('label' => 'Galleries', 'url' => array('/jellyGallery/admin'), 'visible' => !Yii::app()->user->isGuest),                      
 				array('label' => 'Products', 'url' => 'https://plugin.wireflydesign.com/product/backend.php/site/testDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
-				array('label' => 'Blog', 'url' => 'https://plugin.wireflydesign.com/blog/backend.php/site/testDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
+				array('label' => 'News', 'url' => 'https://plugin.wireflydesign.com/news/backend.php/site/testDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Events', 'url' => 'https://plugin.wireflydesign.com/event/backend.php/site/testDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Tickets', 'url' => 'https://plugin.wireflydesign.com/ticket2/backend.php/site/testDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
                 array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
