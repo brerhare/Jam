3ae40b2676ae1c0338e712187f26b92f3bf23e85
root on bo <kim@wireflydesign.com>
2014-10-20 10:53:10 +0100

    Demo3 - CK

--- www/yii/jelly_skeleton/protected/backend/config/main.php
+++ www/yii/jelly_skeleton/protected/backend/config/main.php
@@ -108,7 +108,7 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@wireflydesign.com',
-		// CKEditor size for page content editing
+		// CKEditor size for page content editing (910 max)
 		'editorpagewidth'=>'<editorpagewidth>',
 		'editorpageheight'=>'<editorpageheight>',
 	),
