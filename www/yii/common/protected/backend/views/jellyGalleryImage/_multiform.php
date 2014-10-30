010dd93abaa8cfde43970aa29cd9d13bf4b081df
root on bo <kim@wireflydesign.com>
2014-10-30 19:28:20 +0000

    Addon - gallery - change file uploader to allow multiple file selection

--- www/yii/common/protected/backend/views/jellyGalleryImage/_multiform.php
+++ www/yii/common/protected/backend/views/jellyGalleryImage/_multiform.php
@@ -4,25 +4,35 @@
 	'htmlOptions'=>array('enctype'=>'multipart/form-data'),
 )); ?>
 
-<br/>
 
-<?php
-$this->widget('CMultiFileUpload',array(
-    'name'=>'files',
-    'accept'=>'jpg|jpeg|gif|png',
-    'max'=>50,
-    'remove'=>Yii::t('ui','Remove'),
-    //'denied'=>'', message that is displayed when a file type is not allowed
-    //'duplicate'=>'', message that is displayed when a file appears twice
-    'htmlOptions'=>array('size'=>25),
-));
-?>
+	<!-- Uploadify starts -->
+    <link rel="stylesheet" type="text/css" href="/js/uploadify/uploadify.css">
+    <script type="text/javascript" src="/js/uploadify/jquery.uploadify.js"></script>
+    <script src="/js/uploadify/jquery.uploadify.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+    $(function() {
+        $('#file_upload').uploadify({
+            'swf'      : '/js/uploadify//uploadify.swf',
+            'uploader' : '<?php echo $this->createUrl('jellyGalleryImage/multiCreateUpload');?>',
+            // Your options here
+        });
+    });
+    </script>
+
+	<br/></br/>
+
+	<input type="hidden" name="uploadify" value="uploadify">
+	<input type="file" name="file_upload" id="file_upload" />
+
+	<!-- Uploadify ends -->
+
 
 	<div class="form-actions">
 		<?php $this->widget('bootstrap.widgets.TbButton', array(
 			'buttonType'=>'submit',
 			'type'=>'primary',
-			'label'=>'Upload',
+			'label'=>'Done',
 		)); ?>
 	</div>
 
