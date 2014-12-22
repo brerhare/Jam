fb38774f142fef6184d7a01cfa3f566f1877692f
kim <kim@wireflydesign.com>
2014-12-22 00:32:20 +0000

    angular - stock - initial commit

---
+++ www/angular/stock/src/app/app.module.js
@@ -0,0 +1,5 @@
+
+angular.module('stock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'smart-table'])
+  .controller('appCtrl', function ($scope) {
+  });
+
---
+++ www/angular/stock/src/app/app.routes.js
@@ -0,0 +1,52 @@
+
+/*
+ * 'state' is the the sref link itself eg 'customer.invoices'
+ * 'url' is for display/accept on the url-bar only, Can be anything
+ * States are nested. The page is drawn from root to leaf, ie 'customer.maintain' first renders 'customer', then 'maintain' into its ui-view
+ */
+
+angular.module('stock')
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('loggedOut', {
+        url: '/',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      })
+
+      .state('home.customer-list', {
+        url: '/customers',
+        templateUrl: 'app/home/customer/list/list.html',
+        controller: 'CustomerListCtrl'
+      })
+      .state('home.customer-invoice', {
+        url: '/invoices',
+        templateUrl: 'app/home/customer/customerInvoice.html',
+        controller: 'CustomerInvoiceCtrl'
+      })
+      .state('home.customer-return', {
+        url: '/anything-I-like-here/or-even-there',
+        templateUrl: 'app/home/customer/customerReturn.html',
+        controller: 'CustomerReturnCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  });
+
+
+/*****
+//angular.module('stock')   // This isn't necessary here
+  .controller('MainMenuCtrl', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+      $scope.classVal=$location.path();
+      return '/' + viewLocation === $location.path() ? 'active' : '';
+    };
+
+  });
+*****/
+
