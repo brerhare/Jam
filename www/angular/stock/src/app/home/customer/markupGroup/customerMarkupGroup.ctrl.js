4d23c09f97a3ae2fde67ebf7309bc6231c74c6d7
root on bo <kim@wireflydesign.com>
2014-12-31 15:21:42 +0000

    stock - in place editing wip

--- www/angular/stock/src/app/home/customer/markupGroup/customerMarkupGroup.ctrl.js
+++ www/angular/stock/src/app/home/customer/markupGroup/customerMarkupGroup.ctrl.js
@@ -6,107 +6,103 @@ toastr.options.positionClass = 'toast-bottom-right';
 
 angular.module('stock')
 
-  .factory('itemFactory', function ($http) {
-    return {
-        getItems: function () {
-            return $http.get(url);
-        },
-        addItem: function (item) {
-            return $http.post(url, item);
-        },
-        deleteItem: function (item) {
-            return $http.delete(url + item.id);
-        },
-        updateItem: function (item) {
-            return $http.put(url + item.id, item);
-        }
-    };
-})
-        
-  .factory('notificationFactory', function () {
-    return {
-        success: function () {
+	.factory('itemFactory', function ($http) {
+		return {
+			getItems: function () {
+				return $http.get(url);
+			},
+			addItem: function (item) {
+				return $http.post(url, item);
+			},
+			deleteItem: function (item) {
+				return $http.delete(url + item.id);
+			},
+			updateItem: function (item) {
+				return $http.put(url + item.id, item);
+			}
+		};
+	})
+
+	.factory('notificationFactory', function () {
+		return {
+			success: function () {
 alert('success - hold for log');
-            toastr.success("Success");
-        },
-        error: function (text) {
+				toastr.success("Success");
+			},
+			error: function (text) {
 alert('error - hold for log');
-            toastr.error(text, "Error!");
-        }
-    };
-})
+				toastr.error(text, "Error!");
+			}
+		};
+	})
 
-  .controller('CustomerMarkupGroupCtrl', function ($scope, itemFactory, notificationFactory) {
-    $scope.items = [];
-    $scope.addMode = false;
+	.controller('CustomerMarkupGroupCtrl', function ($scope, itemFactory, notificationFactory) {
+		$scope.items = [];
+		$scope.addMode = false;
  
-    $scope.toggleAddMode = function () {
-        $scope.addMode = !$scope.addMode;
-    };
+		$scope.toggleAddMode = function () {
+			$scope.addMode = !$scope.addMode;
+		};
  
-    $scope.toggleEditDescription = function (item) {
-        item.editDescription = !item.editDescription;
-    };
- 
-    $scope.toggleEditPercent = function (item) {
-        item.editPercent = !item.editPercent;
-    };
- 
-	$scope.editDescriptionEnd = function(item){
-		if (event.keyCode == 27 )
-			$scope.toggleEditDescription();
-		if (event.keyCode == 13 && item.description){
-			$scope.toggleEditDescription();
-		}
-  	};
+		$scope.uneditAllBut = function(item) {
+			for (var i = 0; i < $scope.items.length; i++) {
+				if ($scope.items[i] != item) {
+					$scope.items[i].editDescription = false;
+					$scope.items[i].editPercent = false;
+				}
+			};
+		};
 
-    var getItemsSuccessCallback = function (data, status) {
-        $scope.items = data;
-    };
- 
-    var successCallback = function (data, status, headers, config) {
-        notificationFactory.success();
-        return itemFactory.getItems().success(getItemsSuccessCallback).error(errorCallback);
-    };
- 
-    var successPostCallback = function (data, status, headers, config) {
-        successCallback(data, status, headers, config).success(function () {
-            $scope.toggleAddMode();
-            $scope.item = {};
-        });
-    };
- 
-    var errorCallback = function (data, status, headers, config) {
-        notificationFactory.error(data.ExceptionMessage);
-    };
- 
-    itemFactory.getItems().success(getItemsSuccessCallback).error(errorCallback);
- 
-    $scope.addItem = function () {
-        itemFactory.addItem($scope.item).success(successPostCallback).error(errorCallback);
-    };
- 
-    $scope.deleteItem = function (item) {
-        itemFactory.deleteItem(item).success(successCallback).error(errorCallback);
-    };
- 
-    $scope.updateItem = function (item) {
-        itemFactory.updateItem(item).success(successCallback).error(errorCallback);
-    };
-});
+		$scope.toggleEditDescription = function (item) {
+			item.editDescription = !item.editDescription;
+			if (item.editDescription)
+				$scope.uneditAllBut(item);
+		};
 
-/***
-app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
+		$scope.toggleEditPercent = function (item) {
+			item.editPercent = !item.editPercent;
+		};
+ 
+		$scope.editDescriptionEnd = function(keyEvent, item) {
+			if (event.keyCode == 13 && item.description){
+				$scope.toggleEditDescription(item);
+			}
+		};
 
-                event.preventDefault();
-            }
-        });
-    };
-});
-***/
+// ----------------------------------------------------------------------------------------
+
+		var getItemsSuccessCallback = function (data, status) {
+			$scope.items = data;
+		};
+ 
+		var successCallback = function (data, status, headers, config) {
+			notificationFactory.success();
+			return itemFactory.getItems().success(getItemsSuccessCallback).error(errorCallback);
+		};
+ 
+		var successPostCallback = function (data, status, headers, config) {
+			successCallback(data, status, headers, config).success(function () {
+				$scope.toggleAddMode();
+				$scope.item = {};
+			});
+		};
+ 
+		var errorCallback = function (data, status, headers, config) {
+			notificationFactory.error(data.ExceptionMessage);
+		};
+ 
+		itemFactory.getItems().success(getItemsSuccessCallback).error(errorCallback);
+ 
+		$scope.addItem = function () {
+			itemFactory.addItem($scope.item).success(successPostCallback).error(errorCallback);
+		};
+ 
+		$scope.deleteItem = function (item) {
+			itemFactory.deleteItem(item).success(successCallback).error(errorCallback);
+		};
+ 
+		$scope.updateItem = function (item) {
+			itemFactory.updateItem(item).success(successCallback).error(errorCallback);
+		};
+	});
+
