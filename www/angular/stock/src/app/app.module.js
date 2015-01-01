d712ef226e5f9d4c3dabb110c0e03c0f76da29b8
Kim <kim@wireflydesign.com>
2015-01-01 00:49:20 +0000

    stock - moved factories from customerMarkupGtoup.ctrl.js to app.module.js

--- www/angular/stock/src/app/app.module.js
+++ www/angular/stock/src/app/app.module.js
@@ -1,4 +1,37 @@
 angular.module('stock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'smart-table'])
+
+	.factory('itemFactory', function ($http) {
+		return {
+			getItem: function (id) {
+				id = typeof id !== 'undefined' ? id : "";
+				return $http.get(url + id);
+			},
+			addItem: function (item) {
+				return $http.post(url, item);
+			},
+			deleteItem: function (item) {
+				return $http.delete(url + item.id);
+			},
+			updateItem: function (item) {
+				return $http.put(url + item.id, item);
+			}
+		};
+	})
+
+	.factory('notificationFactory', function () {
+		return {
+			success: function () {
+//alert('success - hold for log');
+				toastr.success("Success");
+			},
+			error: function (text) {
+//alert('error - hold for log');
+				toastr.error(text, "Error!");
+			}
+		};
+	})
+
+
   .controller('appCtrl', function ($scope) {
   });
 
--- www/angular/stock/src/app/home/customer/markupGroup/customerMarkupGroup.ctrl.js
+++ www/angular/stock/src/app/home/customer/markupGroup/customerMarkupGroup.ctrl.js
@@ -5,38 +5,6 @@ toastr.options.positionClass = 'toast-bottom-right';
 //toastr.options.progressBar = true; 
 
 angular.module('stock')
-
-	.factory('itemFactory', function ($http) {
-		return {
-			getItem: function (id) {
-				id = typeof id !== 'undefined' ? id : "";
-				return $http.get(url + id);
-			},
-			addItem: function (item) {
-				return $http.post(url, item);
-			},
-			deleteItem: function (item) {
-				return $http.delete(url + item.id);
-			},
-			updateItem: function (item) {
-				return $http.put(url + item.id, item);
-			}
-		};
-	})
-
-	.factory('notificationFactory', function () {
-		return {
-			success: function () {
-//alert('success - hold for log');
-				toastr.success("Success");
-			},
-			error: function (text) {
-//alert('error - hold for log');
-				toastr.error(text, "Error!");
-			}
-		};
-	})
-
 	.controller('CustomerMarkupGroupCtrl', function ($scope, itemFactory, notificationFactory) {
 		$scope.items = [];
 		$scope.addMode = false;
