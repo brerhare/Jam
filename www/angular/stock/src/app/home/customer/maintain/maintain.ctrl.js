200c0bd617e23b436f44748e352c31f6dc06bee2
kim <kim@wireflydesign.com>
2015-01-02 18:53:34 +0000

    stock - begin add/edit, add fade

--- www/angular/stock/src/app/home/customer/maintain/maintain.ctrl.js
+++ www/angular/stock/src/app/home/customer/maintain/maintain.ctrl.js
@@ -3,6 +3,25 @@ angular.module('stock')
 		var url = 'http://stock.wireflydesign.com/server/api/stock_customer/';
 
 		$scope.rowCollection = [];
+		$scope.displayMode = "list";
+		$scope.formMode = "";
+
+		$scope.addItem = function()
+		{
+			$scope.displayMode = "form";
+			$scope.formMode = "add";
+		}
+
+		$scope.editItem = function()
+		{
+			$scope.displayMode = "form";
+			$scope.formMode = "edit";
+		}
+
+		$scope.saveItem = function()
+		{
+			$scope.displayMode = "list";
+		}
 
 		var getItemSuccessCallback = function (data, status) {
 			$scope.rowCollection = data;
@@ -16,12 +35,12 @@ angular.module('stock')
 		restFactory.getItem(url).success(getItemSuccessCallback).error(errorCallback);
 	})
 
-	.controller('CustomerAddCtrl', function($scope, $state, $stateParams) {
+/*	.controller('CustomerAddCtrl', function($scope, $state, $stateParams) {
 		$scope.addItem = function() {
 			//$scope.rowCollection.push({'id': 999, 'name':'kim', 'discount_percent':100, 'telephone':'07899752030', 'forma_de_pago':'el-nino'});
 			alert('added customer');
 			$state.go('home.customer-maintain');
 		};
 
-	});
+	}); */
 
