d1ddbd29a0464effe8b0b216a1c1bda22cc2bcd0
kim <kim@wireflydesign.com>
2015-01-07 08:52:02 +0000

    stock - before split toggleEditName for double-click = edit vs choose mode

--- www/angular/stock/src/app/home/product/group/productGroup.ctrl.js
+++ www/angular/stock/src/app/home/product/group/productGroup.ctrl.js
@@ -15,6 +15,7 @@ angular.module('stock')
 			$scope.levels[i] = {};
 			$scope.levels[i].parent = null;
 			$scope.levels[i].addMode = false;
+			$scope.levels[i].selectedItem = -1;		// There are 2 'selected' fields. This one to quickly indicate the ix of the selected item, and a 'selected' property within each item for html class checking
 			$scope.levels[i].items = {};
 			$scope.levels[i].levelNo = i;
 		}
@@ -37,6 +38,18 @@ console.log($scope.levels);
 		$scope.toggleEditName = function (level, item) {
 			if (!$scope.addMode)
 			{
+				for (var i = 0; i < $scope.levels[level].items.length; i++) {
+					if ($scope.levels[level].items[i] == item) {
+						item.selected = true;
+						$scope.levels[level].selected = i;
+					}
+					else {
+						$scope.levels[level].items[i].selected = false;
+					}
+				}
+				item.selected = true;
+
+return;
 				item.editName = !item.editName;
 				if (item.editName)
 					uneditAllBut(level, item);
@@ -53,9 +66,22 @@ console.log($scope.levels);
 // ----------------------------------------------------------------------------------------
 
 		var getItemSuccessCallback = function (data, status) {
-			$scope.levels[ajaxLevel].items = data;
+			$scope.levels[ajaxLevel].items = [];
+			console.log(data);
+			for (var i = 0; i < data.length; i++) {
+				if (ajaxLevel == 0) {									// top level
+					if (data[i].parent_id == null)	{
+						$scope.levels[ajaxLevel].items.push(data[i]);
+					}
+				}
+				else {													// not top level
+					if (data[i].parent_id == $scope.levels[ajaxLevel-1].item[$scope.levels[ajaxLevel-1].selected])
+						$scope.levels[ajaxLevel].items.push(data[i]);
+				}
+			}
+			//$scope.levels[ajaxLevel].items = data;
 		};
- 
+
 // ----------------------------------------------------------------------------------------
 
 		var successCallback = function (data, status, headers, config) {
