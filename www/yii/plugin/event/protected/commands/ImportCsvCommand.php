e402853c16a17ecbb639e8160ee2c116bd6101b6
root on bo <kim@wireflydesign.com>
2015-01-12 15:13:38 +0000

    plugin - event - import working through commandline

--- www/yii/plugin/event/protected/commands/ImportCsvCommand.php
+++ www/yii/plugin/event/protected/commands/ImportCsvCommand.php
@@ -4,6 +4,7 @@ class ImportCsvCommand extends CConsoleCommand
 {
 	public function run($args)
 	{
+		$dummyRun = 0;
 		$file = "/tmp/ws.csv";
 		$row = 0;
 		if (($handle = fopen($file, "r")) === FALSE)
@@ -13,20 +14,24 @@ class ImportCsvCommand extends CConsoleCommand
         	// Ignore header line
         	if ($row == 0)
         	{
+				echo $row . " --> " . print_r($data, true) . "\n";
             	$row++;
            		continue;
         	}
-        	if ($row == 1)
-        	{
-        		$row++;
-        		continue;
-        	}
+        	//if ($row == 1)
+        	//{
+        		//$row++;
+        		//continue;
+        	//}
 
-        	// Init db fields
+        	// Init db tables;
         	$event = new Event;
         	$ws = new Ws;
+			$eventHasInterest = new EventHasInterest;
+			$eventHasFormat = new EventHasFormat;
 
         	$num = count($data);
+			echo $row . " --> ";
         	//echo "<p> $num fields in line $row: <br /></p>\n";
         	for ($c = 0; $c < $num; $c++)
         	{
@@ -115,17 +120,36 @@ class ImportCsvCommand extends CConsoleCommand
 						break;
 				}
 			}
-// @@TODO: REMOVE HARD CODING!
-			//$event->member_id = 7;
-			$event->program_id = 6;
-			//if (!($event->save()))
-				//die("Event save failed on line " . $row);
+
+// @@TODO: Remove hardcoding in all the updates here
+			$event->member_id = 25;				// $member->id;
+			$event->program_id = 6;				// $member->lock_program_id;
+			$event->event_price_band_id = 3;	// whatever value was input
+			$event->active = 1;
+			if ((!($dummyRun)) && (!($event->save())))
+				die( "\nEvent save failed on line " . $row . "\n");
+
 			$ws->event_id = $event->id;
 			echo $ws->event_id . "<br>";
-			//if (!($ws->save()))
-				//die("Event additional info save failed on line " . $row);
+			if ((!($dummyRun)) && (!($ws->save())))
+				die("\nEvent additional info save failed on line " . $row . "\n");
+
+			$eventHasInterest->event_event_id = $event->id;
+			$eventHasInterest->event_interest_id = 11;
+			if ((!($dummyRun)) && (!($eventHasInterest->save())))
+				die("\nEvent has interest save failed on line " . $row . "\n");
+
+			$eventHasFormat->event_event_id = $event->id;
+			$eventHasFormat->event_format_id = 2;
+			if ((!($dummyRun)) && (!($eventHasFormat->save())))
+				die("\nEvent has format save failed on line " . $row . "\n");
+
+			echo " UPDATED";
+
 			$row++;
+			echo "\n";
 		}
+		echo ($row-1) . " rows imported\n";
 	}
 
 }
