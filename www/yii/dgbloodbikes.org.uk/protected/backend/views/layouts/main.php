149d4632658d2810a5a1113c12905ca050a62e4c
root on bo <kim@wireflydesign.com>
2015-02-18 11:04:12 +0000

    dgbloocbikes - plugins

--- www/yii/dgbloodbikes.org.uk/protected/backend/views/layouts/main.php
+++ www/yii/dgbloodbikes.org.uk/protected/backend/views/layouts/main.php
@@ -39,13 +39,14 @@
                 //array('label' => 'HTML Sliders', 'url' => array('/jellySliderHtml/admin'), 'visible' => !Yii::app()->user->isGuest),
                 //array('label' => 'Tabs', 'url' => array('/tabBlock/admin'), 'visible' => !Yii::app()->user->isGuest),                      
 				array('label' => 'Mailer', 'url' => 'https://plugin.wireflydesign.com/mailer/backend.php/site/bloodbikesDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
-				array('label' => 'News', 'url' => 'https://plugin.wireflydesign.com/News/backend.php/site/bloodbikesDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
+				array('label' => 'News', 'url' => 'https://plugin.wireflydesign.com/news/backend.php/site/bloodbikesDirect', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Galleries', 'url' => array('/jellyGallery/admin'), 'visible' => !Yii::app()->user->isGuest),
                 array('label' => 'Downloads', 'url' => '#', 'visible' => !Yii::app()->user->isGuest,
                 'items' => array(
                     array('label' => 'Files', 'url' => array('/jellyDownloadFile/admin'), 'visible' => !Yii::app()->user->isGuest),
                     array('label' => 'Collections', 'url' => array('/jellyDownloadCollection/admin'), 'visible' => !Yii::app()->user->isGuest),
                 )),
+				array('label' => 'Ad Blocks', 'url' => array('/jellyAdblock/admin'), 'visible' => !Yii::app()->user->isGuest),
 				array('label' => 'Settings', 'url' => array('/jellySetting/admin'), 'visible' => !Yii::app()->user->isGuest),
 
                 array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
