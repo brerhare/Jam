3c181bf08fd8177fd4f3837d498982208c89aadc
root on bo <kim@wireflydesign.com>
2015-03-11 15:39:15 +0000

    addon - slider - thumbelina - add zoom

--- www/yii/common/scripts/jelly-0.1.4/addon/slider/thumbelina/thumbelina.php
+++ www/yii/common/scripts/jelly-0.1.4/addon/slider/thumbelina/thumbelina.php
@@ -5,7 +5,7 @@
  *
  * Notes
  * -----
- * None
+ * This includes a separate package - jquery zoom - to show the selected image
  */
 
 class thumbelina
@@ -99,44 +99,80 @@ class thumbelina
 
 	private $apiHtml = <<<END_OF_API_HTML
 
-        <style>
-        #thumbelinaSliderControl {
-            position:relative;
-            margin-top:40px;
-            width:93px;
-            height:256px;
-            border-left:1px solid #aaa;
-            border-right:1px solid #aaa;
-            margin-bottom:40px;
-        }
-        .thumbelinaThumb {
-            width:80px;
-            Xheight:80px;
-        }
-        </style>
-
         <div id="jelly-thumbelina-container">
+
             <!--Thumbelina Slider-->
             <link rel="stylesheet" href="<substitute-path>/thumbelina.css" type="text/css">
             <script src="<substitute-path>/thumbelina.js"></script>
-
+            <script src="<substitute-path>/jqueryZoom/jquery.zoom.js" type="text/javascript"></script>
 			
-            <!-- <div id="thumbelinaSliderFrame" style="height:<substitute-height>; width:<substitute-width>; overflow:hidden"> -->
+            <style>
+            #thumbelinaSliderControl {
+                position:relative;
+                margin-top:40px;
+                width:93px;
+                height:256px;
+                border-left:1px solid #aaa;
+                border-right:1px solid #aaa;
+                margin-bottom:40px;
+            }
+            .thumbelinaThumb {
+                width:80px;
+                Xheight:80px;
+            }
+            </style>
+
+            <style>
+            .zoomIcon {
+                width:33px;
+                height:33px;
+                position:absolute;
+                top:0;
+                right:0;
+                background:url(/zoom/icon.png);
+            }
+            .zoom {
+                display:inline-block;
+                position:relative;
+            }
+            .zoom img {
+                display: block;
+                max-width:none;
+            }
+            .zoom p {
+                position:absolute;
+                top:3px;
+                right:28px;
+                color:#555;
+                font:bold 13px/1 sans-serif;
+            }
+            </style>
+
             <div id="thumbelinaSliderFrame" style="height:auto; overflow:hidden">
-                <div  id="thumbelinaSliderControl">
+                <div  id="thumbelinaSliderControl" style="float:left">
                     <div class="thumbelina-but <substitute-A>">&#708;</div>
                         <ul>
                             <substitute-data>
                         </ul>
                     <div class="thumbelina-but <substitute-B>">&#709;</div>
                 </div>
-            <div>
+
+				<!-- jquery-zoom -->
+                <div id="zoom-block" style="float:left">
+                    <span class='zoom' id='zoom-container'>
+                        <p>Hover</p>
+                        <img src="/userdata/jelly/sliderimage/poppy bowl.jpg" height='200' id='zoom-block-image'/>
+                    </span>
+                </div>
+
+            </div>    <!-- thumbelinaSliderFrame -->
+        </div>    <!-- jelly-thumbelina-container -->
 
 END_OF_API_HTML;
 
 	private $apiJs = <<<END_OF_API_JS
 
-	// Any custom js and/or startup functions
+	// Thumbelina stuff
 
     $(function(){
         $('#thumbelinaSliderControl').Thumbelina({
@@ -145,6 +181,30 @@ END_OF_API_HTML;
             \$fwdBut:$('#thumbelinaSliderControl .bottom')   // Selector to bottom button.
         });
     })
+
+	// Jquery-zoom stuff
+
+	$(document).ready(function(){
+    	$("#zoom-block span img").hover(function(){
+        	$('#zoom-block-image').attr({src: $(this).attr('alt')});
+        	$('#zoom-container').zoom();
+    	});
+    	$('#zoom-container').zoom();
+    	var imgSwap = [];
+    	$("#zoom-block li img").each(function(){
+        	imgUrl = this.alt;
+        	imgSwap.push(imgUrl);
+    	});
+    	$(imgSwap).preload();
+	});
+
+	$.fn.preload = function(){
+    	this.each(function(){
+        	$('<img/>')[0].src = this;
+    	});
+	}
+
+
 
 END_OF_API_JS;
 
