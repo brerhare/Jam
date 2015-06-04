0421d7b305fe3c23baf21dcc40648511405a15db
root on bo <kim@wireflydesign.com>
2015-06-04 14:44:32 +0100

    patch - initial

--- www/yii/tools/add_meta_fields_to_content_block.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE content_block ADD meta_title TEXT AFTER home;
-ALTER TABLE content_block ADD meta_description TEXT AFTER meta_title;
-ALTER TABLE content_block ADD meta_keywords TEXT AFTER meta_description;
---
+++ www/yii/tools/patch.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'jellySites.inc';
+
+if (defined('STDIN')) 	// ie running in commandline mode
+{
+	// Running from CLI 
+	if (!isset($argv[1]))
+		die("Needs patch file as argument\n");
+	$patchFile = $argv[1];
+	if (!file_exists($patchFile))
+		die("Patch file $patchFile missing\n");
+	require($patchFile);
+
+	foreach ($siteList as $domain => $location):
+		//echo "Patching $domain\n";
+		patch($domain, $location);
+	endforeach;
+}
+else
+	echo "Nothing happening here - this needs to be run in commandline mode\n";
+
+?>
