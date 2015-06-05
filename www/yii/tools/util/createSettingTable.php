bfa95fabb2408d44c3b707ed879218e113019ca3
root on bo <kim@wireflydesign.com>
2015-06-05 16:34:14 +0100

    createsite - default menus

--- www/yii/tools/util/createSettingTable.php
+++ www/yii/tools/util/createSettingTable.php
@@ -10,9 +10,17 @@ CREATE  TABLE IF NOT EXISTS `jelly_setting` (
 ENGINE = InnoDB;
 END_OF_COMMANDS;
 
-	//system("ls -d $location");
 	echo "$domain\n";
+
+	system("cd $location; cp /tmp/favicon.ico .");
+
+	system("cd $location/protected/models; ln -s ../../../common/protected/models/JellySetting.php; git add JellySetting.php");
+	system("cd $location/protected/backend/controllers; ln -s ../../../../common/protected/backend/controllers/JellySettingController.php; git add JellySettingController.php");
+	system("cd $location/protected/backend/views; ln -s ../../../../common/protected/backend/views/jellySetting/; git add jellySetting");
+	system("cd $location; git commit -m 'jelly - add settings table'; git pull; git push");
+
 	dbExec($location, $dbCommands);
+
 	echo "------------------------------------------\n";
 
 }
