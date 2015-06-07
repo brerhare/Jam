d99ad55e18d5d13fbee433eda475a24487a8e5a1
root on bo <kim@wireflydesign.com>
2015-06-07 19:26:54 +0100

    jelly - fix up plugin jumpto's

--- www/yii/demo2.wireflydesign.com/protected/backend/views/layouts/main.php
+++ www/yii/demo2.wireflydesign.com/protected/backend/views/layouts/main.php
@@ -52,7 +52,7 @@
                         array('label' => 'Products', 'url' => 'https://plugin.wireflydesign.com/product/backend.php/site/demo2Direct', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
                         array('label' => 'Blog', 'url' => 'https://plugin.wireflydesign.com/news/backend.php/site/demo2Direct', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
                         array('label' => 'Newsletters', 'url' => 'https://plugin.wireflydesign.com/mailer/backend.php/site/demo2Direct', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
-                        array('label' => 'Events', 'url' => 'https://plugin.wireflydesign.com/event/backend.php/site/demo2Direct', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
+                        array('label' => 'Events', 'url' => 'https://plugin.wireflydesign.com/event/backend.php/site/login', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
                         array('label' => 'Tickets', 'url' => 'https://plugin.wireflydesign.com/ticket/backend.php/site/demo2Direct', 'linkOptions' => array("target"=>"_blank"), 'visible' => !Yii::app()->user->isGuest),
                     ),
                 ),
