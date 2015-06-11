7be3ce305706a5dd400187e873f5b4e1367fe20a
root on bo <kim@wireflydesign.com>
2015-06-11 21:38:16 +0100

    jelly - createJellyColumn.php util

--- www/yii/tools/util/createJellyColumn.php
+++ www/yii/tools/util/createJellyColumn.php
@@ -15,24 +15,32 @@ CREATE  TABLE IF NOT EXISTS `jelly_column` (
 ENGINE = InnoDB;
 END_OF_COMMANDS;
 
-	if ($location == "test.wireflydesign.com")
+/*
+not sure why this check doesnt work
+	if (($location == "test.wireflydesign.com")
+	||  ($location == "absoluteclassics.co.uk"))
 	{
 		echo "skipping $domain\n";
 		return;
 	}
+*/
 
 	echo "$domain\n";
 
-	system("cd $location/protected/models/; ln -s ../../../common/protected/models/JellyColumn.php");
-	system("cd $location/protected/models; git add JellyColumn.php");
+	//system("cd $location/protected/models/; ln -s ../../../common/protected/models/JellyColumn.php");
+	//system("cd $location/protected/models; git add JellyColumn.php");
 
-	system("cd $location/protected/backend/controllers/; ln -s ../../../../common/protected/backend/controllers/JellyColumnController.php");
-	system("cd $location/protected/backend/controllers/; git add JellyColumnController.php");
+	//system("cd $location/protected/backend/controllers/; ln -s ../../../../common/protected/backend/controllers/JellyColumnController.php");
+	//system("cd $location/protected/backend/controllers/; git add JellyColumnController.php");
 
-	system("cd $location/protected/backend/views/; ln -s ../../../../common/protected/backend/views/jellyColumn/");
-	system("cd $location/protected/backend/views/; git add jellyColumn/*");
+	//system("cd $location/protected/backend/views/; ln -s ../../../../common/protected/backend/views/jellyColumn/");
+	//system("cd $location/protected/backend/views/; git add jellyColumn");
 
-	dbExec($location, $dbCommands);
+	//dbExec($location, $dbCommands);
+
+system("cd $location/protected/backend/controllers/; git pull");
+system("cd $location/protected/backend/controllers/; git commit -m 'jelly - add jellyColumn'");
+system("cd $location/protected/backend/controllers/; git push");
 
 	echo "------------------------------------------\n";
 
