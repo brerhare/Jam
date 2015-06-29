4999ff5bb5372a563721a7134d820b58e36a6e77
root on bo <kim@wireflydesign.com>
2015-06-29 09:58:58 +0100

    jelly - create jelly_column table

--- www/yii/common/protected/data/jelly.sql
+++ www/yii/common/protected/data/jelly.sql
@@ -221,6 +221,21 @@ CREATE  TABLE IF NOT EXISTS `jelly_adblock` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `jelly_column`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `jelly_column` ;
+
+CREATE  TABLE IF NOT EXISTS `jelly_column` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `column_id` INT NULL ,
+  `sequence` INT NULL ,
+  `title` VARCHAR(255) NOT NULL ,
+  `content` TEXT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `sequence` (`sequence` ASC) )
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
