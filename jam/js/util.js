f912b7f46bc5e124e44909b900046b3ada214364
Kim <kim@wireflydesign.com>
2015-07-17 11:34:05 +0100

    jam - add js

---
+++ jam/js/util.js
@@ -0,0 +1,6 @@
+// Place at end of html to run code after dom loaded but not waiting for images to finish loading
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+//alert('x');
+})();
