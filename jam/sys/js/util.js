1c4f897a7a6cb67c2a4266939259ad5894168299
Kim <kim@wireflydesign.com>
2015-08-06 16:26:46 +0100

    jam - tidy util.js

--- jam/sys/js/util.js
+++ jam/sys/js/util.js
@@ -1,11 +1,21 @@
+/*
+ *  Either just a 'template.tpl' or a '/full/url/to/temaplate.tpl'
+ */
 function runTemplate(templateName) {
-	var currLocation = location.href;
-	var value = currLocation.substring(currLocation.lastIndexOf('/') + 1);
-	var newLocation = currLocation.replace(value, templateName);
+	var newLocation = templateName;
+	if (templateName.indexOf('/') == -1) {
+		currLocation = location.href;
+		var value = currLocation.substring(currLocation.lastIndexOf('/') + 1);
+		var newLocation = currLocation.replace(value, templateName);
+	}
 	window.location.href = newLocation;
 	return(false);
 }
 
+/*
+ * forms can have several space-separated forms
+ * forms and output are optional
+ */
 function runAction(action, forms, output) {
 	if (typeof forms === 'undefined') { forms = ''; }
 	if (typeof output === 'undefined') { output = ''; }
@@ -44,55 +54,10 @@ function runAction(action, forms, output) {
 	return false;
 }
 
-/*
-$("#supplierform").submit(function(e)
-{
-    var postData = $(this).serialize();
-    var formURL = $(this).attr("action");
-    $.ajax(
-    {
-        url : formURL,
-        type: "POST",
-        data : postData,
-        success:function(data, textStatus, jqXHR) 
-        {
-            //data: return data from server
-alert('back with: ' + data);
-        },
-        error: function(jqXHR, textStatus, errorThrown) 
-        {
-            //if fails      
-			alert('back with failure ' + textStatus);
-        }
-    });
-    e.preventDefault(); //STOP default action
-    //e.unbind(); //unbind. to stop multiple form submit.
-});
-*/
-
-/*
-function myFunction(formName) {
-	var kvpairs = [];
-	var form = document.getElementById(formName);
-	for (var i = 0; i < form.elements.length; i++ ) {
-		var e = form.elements[i];
-		kvpairs.push(encodeURIComponent(e.name) + "=" + encodeURIComponent(e.value));
-	}
-	var queryString = kvpairs.join("&");
-	console.log(queryString);
-	return false;
-}*/
-
 // Place at end of html to run code after dom loaded but not waiting for images to finish loading
 (function() {
    // your page initialization code here
    // the DOM will be available here
-
-//	var ajax = new Ajax();
-//	ajax.post('http://localhost/jamcgi/jam', 'template=/jam/template/supplier.tpl').done(function(response, xhr) {
-//	//ajax.get( 'http://localhost/jamcgi/jam?template=/jam/template/supplier.tpl' ).done(function( response, xhr ) {
-//		alert(response);
-//	});
 
 })()
 
