d718290e889beb9f1465b24f543c902628bc2c79
root on bo <kim@wireflydesign.com>
2015-10-26 00:30:31 +0000

    jam - runjam using conf file for path

--- jam/runjam.c
+++ jam/runjam.c
@@ -5,19 +5,25 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <string.h>
+#include <libgen.h>
 
 #define MAXBUFLEN 1000000
 #define JAM_PATH "/home/dev/src/jam/cgi/jam"
 
 main(int argc, char *argv[]) {
 	char *source = (char *) calloc(1, MAXBUFLEN + 1);
+
 	FILE *fp = fopen("runjam.conf", "r");
 	if (fp != NULL) {
 		size_t len = fread(source, sizeof(char), MAXBUFLEN, fp);
 		if (len == 0)
-			fputs("Error reading file", stderr);
-		else
+			fputs("Error reading file", stdout);
+		else {
 			source[++len] = '\0';
+		}
+		char *p;
+		if (p = strchr(source, '\n'))
+			*p = '\0';
 		fclose(fp);
 	} else
 		strcpy(source, JAM_PATH);
