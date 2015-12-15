e0d927a03b240af65d2de56793741a02170b951f
root on bo <kim@wireflydesign.com>
2015-12-15 14:53:53 +0000

    plugin - mailer - add bryce walker

--- www/yii/plugin/mailer/protected/backend/controllers/SiteController.php
+++ www/yii/plugin/mailer/protected/backend/controllers/SiteController.php
@@ -81,6 +81,20 @@ class SiteController extends Controller
     /**
      * Displays the DIRECT login page
      */
+// @@TODO: HARDCODED FOR BRYCEWALKERVENDING ---------- REMOVE
+    public function actionBryceWalkerVendingDirect()
+    {
+        Yii::app()->session['uid'] = 70;
+        $identity = new UserIdentity('bryce@brycewalkervending.com', 'site2plugin');
+        $identity->authenticate();
+        $duration = 3600*24*14; // 14 days
+        Yii::app()->user->login($identity, $duration);
+        $this->redirect(array('site/index'));
+    }
+
+    /**
+     * Displays the DIRECT login page
+     */
 // @@TODO: HARDCODED FOR Joseawright ---------- REMOVE
     public function actionJoSeawrightDirect()
     {
