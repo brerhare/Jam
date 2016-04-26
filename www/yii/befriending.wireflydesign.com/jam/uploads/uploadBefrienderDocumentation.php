eff3b529cbdc6ce3e76c7976d06df20585477540
root on bo <kim@wireflydesign.com>
2016-04-26 07:58:57 +0100

    bef - uploads

--- www/yii/befriending.wireflydesign.com/jam/uploads/uploadBefrienderDocumentation.php
+++ www/yii/befriending.wireflydesign.com/jam/uploads/uploadBefrienderDocumentation.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_FILES['upload_file'])) {
 	$root = $_SERVER["DOCUMENT_ROOT"];
-	if (move_uploaded_file($_FILES['upload_file']['tmp_name'], $root . "/jam/uploads/forms/befriender/documentation/" . $_GET['event'] . "_" . $_FILES['upload_file']['name'])){
+	if (move_uploaded_file($_FILES['upload_file']['tmp_name'], $root . "/jam/uploads/forms/befriender/documentation/" . $_GET['befrienderId'] . "_" . $_FILES['upload_file']['name'])){
 		echo $_FILES['upload_file']['name']. " OK";
 	} else {
 		echo $_FILES['upload_file']['name']. " KO";
