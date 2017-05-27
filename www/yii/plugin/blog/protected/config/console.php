57e6e4ba19ea21037e6f3bc05e216f538981b1cb
root on bo <kim@wireflydesign.com>
2017-05-27 08:58:31 +0100

    blog - new cron command php to archive dgnews-sport older than 3 months

--- www/yii/plugin/blog/protected/config/console.php
+++ www/yii/plugin/blog/protected/config/console.php
@@ -9,21 +9,28 @@ return array(
 	// preloading 'log' component
 	'preload'=>array('log'),
 
+    //@@EG Had to add the next 'import' to load the db
+    // autoloading model and component classes
+    'import'=>array(
+        'application.models.*',
+        'application.components.*',
+    ),
+
 	// application components
 	'components'=>array(
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		/**/
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+            'connectionString' => 'mysql:host=localhost;dbname=plugin',
+            'emulatePrepare' => true,
+            'username' => 'plugin',
+            'password' => 'plugin,',
+            'charset' => 'utf8',
+        ),
+		/**/
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
